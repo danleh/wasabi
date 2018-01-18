23616741c690f08504a8c9bb38f2e36346f12339
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-01-18 13:47:12 +0100

    parse binary WASM file and pretty print with WASM spec code

--- instrumentation/spec/hello.ml
+++ instrumentation/spec/hello.ml
@@ -1,4 +1,4 @@
-open Wasm
+open Wasm;;
 
 (* All files xyz.ml, regardless of directory structure
  * are available as submodules with capital letters.
@@ -9,6 +9,11 @@ open Wasm
  * E.g., /script/run.mli
  *)
 
-let () = Flags.trace := true
-
-let () = Run.trace "hello world"
+let file = "ackermann.wasm" in
+let in_channel = open_in_bin file in
+let num_bytes = in_channel_length in_channel in
+let bytes = Bytes.create num_bytes in
+really_input in_channel bytes 0 num_bytes;
+close_in in_channel;
+let ast = Decode.decode "ackermann" bytes in
+Print.module_ stdout 80 ast;;
