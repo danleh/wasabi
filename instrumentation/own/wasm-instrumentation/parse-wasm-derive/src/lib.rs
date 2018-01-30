b6f598a409b48d07c7bca9f4b205af027724e835
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-01-30 13:48:13 +0100

    implement custom derive for enum variants with fields (by extracting the impl for structs and reusing that)

--- instrumentation/own/wasm-instrumentation/parse-wasm-derive/src/lib.rs
+++ instrumentation/own/wasm-instrumentation/parse-wasm-derive/src/lib.rs
@@ -4,79 +4,83 @@ extern crate syn;
 extern crate quote;
 
 use proc_macro::TokenStream;
-use syn::{DeriveInput, Data, Type, Ident, DataStruct, DataEnum, Fields, FieldsUnnamed, FieldsNamed, Attribute, Meta, MetaNameValue, Lit};
+use syn::{DeriveInput, Data, Type, Ident, DataStruct, DataEnum, Fields, FieldsUnnamed, FieldsNamed, Meta, MetaNameValue, Lit};
+use quote::Tokens;
 
 #[proc_macro_derive(ParseWasm, attributes(tag))]
 pub fn hello_world(input: TokenStream) -> TokenStream {
-    // Parse the input tokens into a syntax tree
     let input: DeriveInput = syn::parse(input).unwrap();
+    let data_name = input.ident;
 
-    let data_name = input.ident.clone();
-    let data = input.data;
-
-    let body = match data {
-
-        // tuple struct
-        Data::Struct(DataStruct { fields: Fields::Unnamed(FieldsUnnamed { unnamed, .. }), .. }) => {
-            let field_tys: Vec<Type> = unnamed.into_iter().map(|field| field.ty).collect();
-            quote! {
-                #data_name(
-                    #( #field_tys::parse(reader)? ),*
-                )
-            }
-        }
-
-        // struct with fields
-        Data::Struct(DataStruct { fields: Fields::Named(FieldsNamed { named, .. }), .. }) => {
-            let (field_names, field_tys): (Vec<Ident>, Vec<Type>) = named.into_iter().map(|field| (field.ident.unwrap(), field.ty)).unzip();
-            quote! {
-                #data_name {
-                    #( #field_names: #field_tys::parse(reader)? ),*
-                }
-            }
-        }
-
-        // enum with unit variants (all tagged)
+    let impl_body = match input.data {
+        Data::Struct(DataStruct { fields, .. }) => recurse_into_fields(data_name, fields),
         Data::Enum(DataEnum { variants, .. }) => {
-            let (variant_attributes, variant_names): (Vec<Vec<Attribute>>, Vec<Ident>) =
-                variants.into_iter().map(|variant| (variant.attrs, variant.ident)).unzip();
-
-            let variant_tags: Vec<u8> = variant_attributes.into_iter().map(|attributes| {
-                if attributes.len() == 1 {
-                    if let Some(Meta::NameValue(MetaNameValue { ident, lit: Lit::Int(lit_int), .. })) = attributes[0].interpret_meta() {
-                        if ident.to_string() == "tag" && lit_int.value() < u8::max_value() as u64 {
-                            return lit_int.value() as u8
+            let variant_tags: Vec<u8> = variants.iter()
+                .map(|variant| {
+                    if variant.attrs.len() == 1 {
+                        if let Some(Meta::NameValue(MetaNameValue { ident, lit: Lit::Int(lit_int), .. })) = variant.attrs[0].interpret_meta() {
+                            if ident.to_string() == "tag" && lit_int.value() < u8::max_value() as u64 {
+                                return lit_int.value() as u8;
+                            }
                         }
                     }
-                }
-                panic!("every enum variant must have exactly one #[tag = <u8>] attribute")
-            }).collect();
+                    panic!("every enum variant must have exactly one #[tag = <u8>] attribute")
+                })
+                .collect();
 
-            // needs to be repeated for quote repetition
-            let data_name_repeated = vec![data_name; variant_names.len()];
+            let variant_create: Vec<Tokens> = variants.into_iter()
+                .map(|variant| recurse_into_fields(variant.ident, variant.fields))
+                .collect();
 
+            // needs to be repeated for quote repetition with #( ... )*
+            let data_name_repeated = vec![data_name; variant_create.len()];
+
+            // match enum variants by tag
             quote! {
                 match reader.read_byte()? {
-                    #( #variant_tags => #data_name_repeated::#variant_names, )*
+                    #( #variant_tags => #data_name_repeated::#variant_create, )*
                     byte => wasm_error(format!("expected tag for #data_name, got 0x{:02x}, ", byte))?
                 }
             }
         }
-
-        // TODO
-        // step 4: custom tags on enum variants
-        // step 5: enum with struct variants
-        _ => unimplemented!()
+        _ => unimplemented!("can only derive(ParseWasm) for structs and enums")
     };
 
     // boilerplate of impl that is the same for any data type
     let impl_ = quote! {
         impl ParseWasm for #data_name {
             fn parse<R: io::Read>(reader: &mut R) -> io::Result<Self> {
-                Ok(#body)
+                Ok(#impl_body)
             }
         }
     };
 
     impl_.into()
+}
+
+// extract handling of fields since it is the same for structs and enums
+fn recurse_into_fields(name: Ident, fields: Fields) -> Tokens {
+    match fields {
+        Fields::Unit => quote!(#name),
+        Fields::Unnamed(FieldsUnnamed { unnamed, .. }) => {
+            let field_tys: Vec<Type> = unnamed.into_iter()
+                .map(|field| field.ty)
+                .collect();
+            quote! {
+                #name(
+                    #( #field_tys::parse(reader)? ),*
+                )
+            }
+        }
+        Fields::Named(FieldsNamed { named, .. }) => {
+            let (field_names, field_tys): (Vec<Ident>, Vec<Type>) = named.into_iter()
+                .map(|field| (field.ident.unwrap(), field.ty))
+                .unzip();
+            quote! {
+                #name {
+                    #( #field_names: #field_tys::parse(reader)? ),*
+                }
+            }
+        }
+    }
 }
