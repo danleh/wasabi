12dcd7cb220233d2f92c86b74ac477262876b3b9
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-02-12 17:26:38 +0100

    move Display trait impl with wasm2wat to Module.wat() method, because it is still not what I want from Display

--- instrumentation/own/wasm-instrumentation/src/display.rs
+++ instrumentation/own/wasm-instrumentation/src/display.rs
@@ -1,26 +1,33 @@
 use ast::Module;
 use binary::WasmBinary;
-use std::fmt::{Display, Error, Formatter, Result};
+//use std::fmt::{Display, Error, Formatter, Result};
 use std::process::Command;
 use tempfile::NamedTempFile;
 
-impl Display for Module {
-    fn fmt(&self, f: &mut Formatter) -> Result {
-        // TODO implement proper own display trait
-        // don't show Leb128 and WithSize
-        // indent each section, vector etc.
-        // but do not indent indices and func types and other short elements
-
-        let mut tmpfile = NamedTempFile::new().map_err(|_| Error)?;
-        self.encode(&mut tmpfile).map_err(|_| Error)?;
+impl Module {
+    pub fn wat(&self) -> String {
+        let mut tmpfile = NamedTempFile::new().unwrap();
+        self.encode(&mut tmpfile).unwrap();
 
         let output = Command::new("wasm2wat")
             .arg(tmpfile.path().as_os_str())
             .output()
-            .map_err(|_| Error)?;
-        let output = String::from_utf8(output.stdout)
-            .map_err(|_| Error)?;
+            .unwrap();
+        let mut buffer = String::from_utf8(output.stdout).unwrap();
+        buffer.push_str(&String::from_utf8(output.stderr).unwrap());
 
-        f.write_str(&output)
+        buffer
     }
 }
+
+// TODO implement proper own display trait:
+// - wat is too high-level (doesn't show empty sections etc)
+// - fmt::Debug is too low-level/annyoing (shows Leb128 and WithSize, always nested)
+// don't show Leb128 and WithSize
+// indent each section, vector etc.
+// but do not indent indices and func types and other short elements
+
+//impl Display for Module {
+//    fn fmt(&self, f: &mut Formatter) -> Result {
+//    }
+//}
