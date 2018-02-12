3978027ce7a8fcc0aaa062c09ea9dbb096361e9e
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-02-12 20:09:57 +0100

    WIP: add mutable global variable to existing Globals section

--- instrumentation/own/wasm-instrumentation/src/instrument.rs
+++ instrumentation/own/wasm-instrumentation/src/instrument.rs
@@ -1,7 +1,12 @@
 use ast::FuncType;
-use ast::Global;
 use ast::Module;
 use ast::Section;
+use ast::Global;
+use ast::GlobalType;
+use ast::ValType;
+use ast::Mut;
+use ast::Instr;
+use ast::Expr;
 
 pub fn identity(_: &mut Module) {}
 
@@ -14,21 +19,37 @@ pub fn add_trivial_function_type(module: &mut Module) {
 }
 
 pub fn count_call_instructions(module: &mut Module) {
-    module.sections.insert(3, Section::Global(Vec::new().into()));
+    // TODO add impl on Vec<Section> with update_or_insert() fn
+    // - always inserts Custom sections at the back
+    // - for all other sections, inserts at the appropriate place
+    // - and merges if there already is a section of that type (what to do with Start section? overwrite?)
 
-//    let globals: &mut Vec<Global> = module.sections.iter_mut()
-//        .find(|ref section| {
-//            if let &Section::Global(_) = section {
-//                true
-//            } else {
-//                false
-//            }
-//        })
+    let mut vec = module.sections.iter_mut().filter_map(|section| match section {
+        &mut Section::Global(ref mut globals) => {
+            Some(&mut globals.content.value)
+        }
+        _ => None,
+    }).last();
+    // FIXME below probably doesnt work because cannot borrow temporary value :/
+    // unwrap_or(&module.sections.push(Global(Vec::new().into()))
 
-    // TODO make convenience function on modules that does
+    if let Some(ref mut globals) = vec {
+        globals.push(Global {
+            type_: GlobalType(ValType::I32, Mut::Var),
+            init: Expr(vec![Instr::I32Const(0.into())]),
+        });
+    }
 
-//    for mut section in module.sections.iter_mut() {}
-//    module
+//    let section_to_insert = Section::Global(Vec::new().into());
+//
+//    let insert_index = module.sections.iter()
+//        .position(|section| section > &section_to_insert)
+//        .unwrap_or(module.sections.len());
+//
+//    module.sections.insert(insert_index, section_to_insert);
+
+    // TODO make sure some sections are not appearing twice, e.g., Global
+    // TODO to fix this: add merging of sections
 }
 
 // TODO implement actual instrumentations:
