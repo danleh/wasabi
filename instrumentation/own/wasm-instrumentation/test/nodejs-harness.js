5f9e2063931f89d1f4e059ce4005fc8e6265d2f0
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-02-13 19:33:16 +0100

    formatting

--- instrumentation/own/wasm-instrumentation/test/nodejs-harness.js
+++ instrumentation/own/wasm-instrumentation/test/nodejs-harness.js
@@ -3,27 +3,27 @@
 const fs = require("fs");
 
 if (typeof WebAssembly !== "object") {
-	console.error("WebAssembly object not available, make sure to run Node version >= 8.");
-	process.exit(1);
+    console.error("WebAssembly object not available, make sure to run Node version >= 8.");
+    process.exit(1);
 }
 
 // 0 is node itself, 1 is the script, only 2 is then the wasm file
 const wasmFilename = process.argv[2];
 if (typeof wasmFilename !== "string") {
-	console.error("No wasm file to execute given, script argument wasm", wasmFilename);
-	process.exit(1);
+    console.error("No wasm file to execute given, script argument wasm", wasmFilename);
+    process.exit(1);
 }
 
 const wasmBinaryBuffer = fs.readFileSync(wasmFilename);
 const wasmModule = new WebAssembly.Module(wasmBinaryBuffer);
 
 const importObject = {
-	imports: {
-		output: console.log,
-	}
+    imports: {
+        output: console.log
+    }
 };
 
-const instance = new WebAssembly.Instance(wasmModule, importObject)
+const instance = new WebAssembly.Instance(wasmModule, importObject);
 
 // instance.exports."start"() is automatically called
 
