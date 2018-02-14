140e5edb46924a244954f03145bce418dc74abc4
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-02-14 18:50:31 +0100

    add add_function_with_type convenience fn

--- instrumentation/own/wasm-instrumentation/src/instrument.rs
+++ instrumentation/own/wasm-instrumentation/src/instrument.rs
@@ -14,12 +14,38 @@ pub fn add_trivial_type(module: &mut Module) {
 }
 
 pub fn count_calls(module: &mut Module) {
-    let bla = add_global(&mut module.sections,
-               Global {
-                   type_: GlobalType(ValType::I32, Mut::Var),
-                   init: Expr(vec![I32Const(0.into()), End]),
-               }
-    );
+    let counter: GlobalIdx = add_global(&mut module.sections,
+                                        Global {
+                                            type_: GlobalType(ValType::I32, Mut::Var),
+                                            init: Expr(vec![I32Const(0.into()), End]),
+                                        });
+
+    let getter: FunctionIdx = add_function_with_type(
+        &mut module.sections,
+        FuncType {
+            params: Vec::new().into(),
+            results: vec![ValType::I32].into(),
+        },
+        Vec::new(),
+        Expr(vec![
+            GetGlobal(counter),
+            End
+        ]));
+
+    let increment: FunctionIdx = add_function_with_type(
+        &mut module.sections,
+        FuncType {
+            params: Vec::new().into(),
+            results: Vec::new().into(),
+        },
+        Vec::new(),
+        Expr(vec![
+            GetGlobal(counter),
+            I32Const(1.into()),
+            I32Add,
+            SetGlobal(counter),
+            End
+        ]));
 }
 
 // TODO implement actual instrumentations:
@@ -28,10 +54,27 @@ pub fn count_calls(module: &mut Module) {
 // - BB counting
 // ...
 
+/// adds an element to the corresponding section if the section exists,
+/// otherwise creates the section with no elements and inserts it then.
 macro_rules! add_section_element {
+    ($function_name: ident, $SectionVariant: ident, $ElementType: ty) => {
+        fn $function_name(sections: &mut Vec<Section>, element: $ElementType) {
+            let empty_section = Section::$SectionVariant(Vec::new().into());
+            let section_idx = sections.iter()
+                .position(|section| discriminant(section) == discriminant(&empty_section));
+            let section_idx = section_idx.unwrap_or_else(|| insert_section(sections, empty_section));
+
+            let section_elements: &mut Vec<_> = if let &mut Section::$SectionVariant(ref mut elements) = &mut sections[section_idx] {
+                &mut elements.content.value
+            } else {
+                unreachable!("we just found or inserted this section, why has it not the expected type?")
+            };
+
+            section_elements.push(element);
+        }
+    };
+    // FIXME can I somehow refactor out the repetition of the fn body!?
     ($function_name: ident, $SectionVariant: ident, $ElementType: ty, $ElementIdxType: ident) => {
-        /// adds an element to the corresponding section if the section exists,
-        /// otherwise creates the section with no elements and inserts it then.
         fn $function_name(sections: &mut Vec<Section>, element: $ElementType) -> $ElementIdxType {
             let empty_section = Section::$SectionVariant(Vec::new().into());
             let section_idx = sections.iter()
@@ -48,11 +91,25 @@ macro_rules! add_section_element {
             section_elements.push(element);
             $ElementIdxType(new_element_idx.into())
         }
-    }
+    };
 }
 
 add_section_element!(add_type, Type, FuncType, TypeIdx);
+add_section_element!(add_function, Function, TypeIdx, FunctionIdx);
+add_section_element!(add_code, Code, WithSize<Function>, FunctionIdx);
 add_section_element!(add_global, Global, Global, GlobalIdx);
+add_section_element!(add_export, Export, Export);
+add_section_element!(add_import, Import, Import);
+
+/// convenience instead of calling add_type, add_function, add_code manually
+fn add_function_with_type(sections: &mut Vec<Section>, ty: FuncType, locals: Vec<Locals>, body: Expr) -> FunctionIdx {
+    let ty_idx = add_type(sections, ty);
+    let func_idx = add_function(sections, ty_idx);
+    let code_idx = add_code(sections, Function { locals: locals.into(), body }.into());
+    assert_eq!(func_idx, code_idx,
+               "function type and code should have same index, were Type and Code sections of the same size beforehand?");
+    func_idx
+}
 
 /// returns the index where the section was inserted
 fn insert_section(sections: &mut Vec<Section>, to_insert: Section) -> usize {
