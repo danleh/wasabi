988ae7b40fed4ae0c454147590c0e9ef22c3d96d
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-02-15 10:46:31 +0100

    move convenience functions on Module to top-level lib.rs

--- instrumentation/own/wasm-instrumentation/src/ast/mod.rs
+++ instrumentation/own/wasm-instrumentation/src/ast/mod.rs
@@ -1,20 +1,2 @@
-use self::lowlevel::Module;
-use binary::WasmBinary;
-
-use std::fs::File;
-use std::io::{self, BufReader, BufWriter};
-use std::path::Path;
-
 pub mod highlevel;
 pub mod lowlevel;
-
-/// convenience for working files (which is the most common io::Read/Write anyway).
-impl Module {
-    pub fn from_file<P: AsRef<Path>>(path: P) -> io::Result<Self> {
-        Self::decode(&mut BufReader::new(File::open(path)?))
-    }
-
-    pub fn to_file<P: AsRef<Path>>(&self, path: P) -> io::Result<usize> {
-        self.encode(&mut BufWriter::new(File::create(path)?))
-    }
-}
--- instrumentation/own/wasm-instrumentation/src/lib.rs
+++ instrumentation/own/wasm-instrumentation/src/lib.rs
@@ -1,12 +1,17 @@
-#![feature(attr_literals, specialization, conservative_impl_trait, test)]
+#![feature(attr_literals, specialization, universal_impl_trait, conservative_impl_trait, test)]
 
 #[macro_use]
 extern crate custom_derive;
 extern crate byteorder;
 extern crate rayon;
 extern crate test;
-extern crate tempfile;
 extern crate walkdir;
+
+use ast::lowlevel::Module;
+use binary::WasmBinary;
+use std::fs::File;
+use std::io::{self, BufReader, BufWriter};
+use std::path::Path;
 
 mod leb128;
 mod ast;
@@ -14,3 +19,14 @@ mod binary;
 mod instrument;
 #[cfg(test)]
 mod tests;
+
+/// convenience for working files (which is the most common io::Read/Write anyway).
+impl Module {
+    pub fn from_file<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        Self::decode(&mut BufReader::new(File::open(path)?))
+    }
+
+    pub fn to_file<P: AsRef<Path>>(&self, path: P) -> io::Result<usize> {
+        self.encode(&mut BufWriter::new(File::create(path)?))
+    }
+}
