3df352b62badeafab17a1e08e784ff6044a09607
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-02-15 09:52:01 +0100

    remove display() and ::display module, can be replaced with wasm-objdump (duh...)

--- instrumentation/own/wasm-instrumentation/src/lib.rs
+++ instrumentation/own/wasm-instrumentation/src/lib.rs
@@ -4,15 +4,13 @@
 extern crate custom_derive;
 extern crate byteorder;
 extern crate rayon;
-extern crate walkdir;
 extern crate test;
 extern crate tempfile;
+extern crate walkdir;
 
 mod leb128;
 mod ast;
 mod binary;
 mod instrument;
-mod display;
-mod utils;
 #[cfg(test)]
 mod tests;
