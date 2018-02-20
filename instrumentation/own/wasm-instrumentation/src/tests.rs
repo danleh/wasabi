25f0732c35bb7c0ad3fd1966deb2c0ff1ae08a25
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-02-20 15:02:54 +0100

    add benchmarks for low-level to high-level AST conversion and back

--- instrumentation/own/wasm-instrumentation/src/tests.rs
+++ instrumentation/own/wasm-instrumentation/src/tests.rs
@@ -1,80 +1,54 @@
-use ast::lowlevel::Module;
 use binary::WasmBinary;
+use ast::*;
 use instrument::*;
 use std::cell::{Cell, RefCell};
+use std::collections::HashMap;
 use std::fs::{create_dir_all, File};
 use std::io::{self, Cursor, Read, sink};
 use std::path::{Path, PathBuf};
 use test::Bencher;
-use ast::lowlevel::FunctionType;
-use ast::lowlevel::ValType;
-use std::collections::HashMap;
 
 /// "main"-like for quick and dirty testing
 #[test]
 #[ignore]
 fn debug() {
-//    let file = "test/input/hello-emcc.wasm";
+    let file = "test/input/hello-manual.wasm";
 //    use std::mem::size_of;
 //    println!("{} bytes", size_of::<Cell<Option<Rc<RefCell<::ast::highlevel::Function>>>>>());
 
-//    let module: ::ast::highlevel::Module = Module::from_file(file).unwrap().into();
+//    let module: highlevel::Module = Module::from_file(file).unwrap().into();
+//    let module: lowlevel::Module = module.into();
 //    println!("{:#?}", module);
 
 //    instrument(&Path::new(file), count_calls, "count-calls").unwrap();
-}
-
-const SIZE: usize = 10;
-
-#[bench]
-fn vector_linear_search(bencher: &mut Bencher) {
-    let vec = vec![
-        FunctionType(vec![], vec![]),
-        FunctionType(vec![ValType::I32], vec![]),
-        FunctionType(vec![ValType::F32], vec![]),
-        FunctionType(vec![ValType::I32], vec![ValType::I32]),
-        FunctionType(vec![ValType::I32, ValType::I32, ValType::I32], vec![ValType::I32]),
-        FunctionType(vec![ValType::F32, ValType::F32, ValType::I32], vec![ValType::I32]),
-    ];
-    bencher.iter(|| {
-        vec.iter().position(|ty| ty == &FunctionType(vec![ValType::I32], vec![ValType::I32]))
-    })
-}
-
-#[bench]
-fn hashmap_search(bencher: &mut Bencher) {
-    let mut map = HashMap::new();
-    map.insert(FunctionType(vec![], vec![]), 0);
-    map.insert(FunctionType(vec![ValType::I32], vec![]), 1);
-    map.insert(FunctionType(vec![ValType::F32], vec![]), 2);
-    map.insert(FunctionType(vec![ValType::I32], vec![ValType::I32]), 3);
-    map.insert(FunctionType(vec![ValType::I32, ValType::I32, ValType::I32], vec![ValType::I32]), 4);
-    map.insert(FunctionType(vec![ValType::F32, ValType::F32, ValType::I32], vec![ValType::I32]), 5);
-
-    bencher.iter(|| {
-        map.get(&FunctionType(vec![ValType::I32], vec![ValType::I32]))
-    })
 }
 
 
 /* Correctness tests */
 
 #[test]
-fn can_decode_valid_wasm() {
+fn can_lowlevel_decode_valid_wasm() {
     for path in wasm_files("test/input") {
-        Module::from_file(&path).expect(&format!("could not decode valid wasm file {}", path.display()));
+        lowlevel::Module::from_file(&path).expect(&format!("could not decode valid wasm file {} to low-level AST", path.display()));
     }
 }
 
 #[test]
-fn identity_instrumentation_produces_valid_wasm() {
+fn can_highlevel_decode_valid_wasm() {
     for path in wasm_files("test/input") {
-        let output = instrument(&path, identity, "identity").unwrap();
-        wasm_validate(&output).unwrap();
+        highlevel::Module::from_file(&path).expect(&format!("could not decode valid wasm file {} to high-level AST", path.display()));
     }
 }
 
 // FIXME
+//#[test]
+//fn identity_instrumentation_produces_valid_wasm() {
+//    for path in wasm_files("test/input") {
+//        let output = instrument(&path, identity, "identity").unwrap();
+//        wasm_validate(&output).unwrap();
+//    }
+//}
+//
 //#[test]
 //fn add_trivial_type_instrumentation_produces_valid_wasm() {
 //    for path in wasm_files("test/input") {
@@ -94,23 +68,54 @@ fn identity_instrumentation_produces_valid_wasm() {
 
 /* Test encoding/decoding speed (without any instrumentation) on "large" wasm file (~2MB) */
 
+const LARGE_WASM_FILE: &'static str = "test/input/bananabread/bb.wasm";
+
 #[bench]
-fn decode_speed(bencher: &mut Bencher) {
+fn decode_lowlevel_speed(bencher: &mut Bencher) {
     let mut buf = Vec::new();
-    File::open("test/input/bananabread/bb.wasm").unwrap().read_to_end(&mut buf).unwrap();
+    File::open(LARGE_WASM_FILE).unwrap().read_to_end(&mut buf).unwrap();
+
+    bencher.iter(||
+        lowlevel::Module::decode(&mut Cursor::new(&buf)).unwrap())
+}
+
+#[bench]
+fn encode_lowlevel_speed(bencher: &mut Bencher) {
+    let module = lowlevel::Module::from_file(LARGE_WASM_FILE).unwrap();
+
+    bencher.iter(||
+        module.encode(&mut sink()).unwrap())
+}
+
+#[bench]
+fn convert_lowlevel_to_highlevel_speed(bencher: &mut Bencher) {
+    let module = lowlevel::Module::from_file(LARGE_WASM_FILE).unwrap();
 
     bencher.iter(|| {
-        Module::decode(&mut Cursor::new(&buf)).unwrap();
+        let _: highlevel::Module = module.clone().into();
     })
 }
 
 #[bench]
-fn encode_speed(bencher: &mut Bencher) {
-    let module = Module::from_file("test/input/bananabread/bb.wasm").unwrap();
+fn convert_highlevel_to_lowlevel_speed(bencher: &mut Bencher) {
+    let module = highlevel::Module::from_file(LARGE_WASM_FILE).unwrap();
 
     bencher.iter(|| {
-        module.encode(&mut sink()).unwrap();
+        let _: lowlevel::Module = module.clone().into();
     })
+}
+
+// as baseline for conversions high-level <-> low-level (where we need to clone -.-)
+#[bench]
+fn clone_lowlevel_module_speed(bencher: &mut Bencher) {
+    let module = lowlevel::Module::from_file(LARGE_WASM_FILE).unwrap();
+    bencher.iter(|| module.clone())
+}
+
+#[bench]
+fn clone_highlevel_module_speed(bencher: &mut Bencher) {
+    let module = highlevel::Module::from_file(LARGE_WASM_FILE).unwrap();
+    bencher.iter(|| module.clone())
 }
 
 
@@ -124,16 +129,17 @@ fn wasm_files<P: AsRef<Path>>(dir: P) -> impl Iterator<Item=PathBuf> {
         .filter(|path| path.extension() == Some("wasm".as_ref()))
 }
 
-fn instrument(test_file: &Path, instrument: impl Fn(&mut Module), instrument_str: &str) -> io::Result<PathBuf> {
+fn instrument(test_file: &Path, instrument: impl Fn(&mut highlevel::Module), instrument_str: &str) -> io::Result<PathBuf> {
     assert!(test_file.to_string_lossy().contains("test/input"),
             "otherwise creating the output file and directories could fail/overwrite other stuff");
     let output_dir = "output/".to_string() + instrument_str;
     let output_file = PathBuf::from(test_file.to_string_lossy().replace("input", &output_dir));
     create_dir_all(output_file.parent().unwrap_or(&output_file))?;
 
-    let mut module = Module::from_file(test_file)?;
-    instrument(&mut module);
-    module.to_file(&output_file)?;
+    // FIXME should instrumentation work on highlevel::Module? -> change signature of instrument()
+//    let mut module = Module::from_file(test_file)?;
+//    instrument(&mut module);
+//    module.to_file(&output_file)?;
     Ok(output_file)
 }
 
