afbf48a4e33a6d212b29b774d09601ad7f32c5ef
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-02-20 15:03:18 +0100

    add convenience impl for reading/writing modules to high-level AST

--- instrumentation/own/wasm-instrumentation/src/lib.rs
+++ instrumentation/own/wasm-instrumentation/src/lib.rs
@@ -1,14 +1,14 @@
 #![feature(attr_literals, specialization, universal_impl_trait, conservative_impl_trait, test)]
 #![allow(dead_code, unused_variables, unused_imports)]
 
+extern crate byteorder;
 #[macro_use]
 extern crate custom_derive;
-extern crate byteorder;
 extern crate rayon;
 extern crate test;
 extern crate walkdir;
 
-use ast::lowlevel::Module;
+use ast::{highlevel, lowlevel};
 use binary::WasmBinary;
 use std::fs::File;
 use std::io::{self, BufReader, BufWriter};
@@ -22,12 +22,23 @@ mod instrument;
 mod tests;
 
 /// convenience for working files (which is the most common io::Read/Write anyway).
-impl Module {
+impl lowlevel::Module {
     pub fn from_file<P: AsRef<Path>>(path: P) -> io::Result<Self> {
         Self::decode(&mut BufReader::new(File::open(path)?))
     }
 
     pub fn to_file<P: AsRef<Path>>(&self, path: P) -> io::Result<usize> {
         self.encode(&mut BufWriter::new(File::create(path)?))
+    }
+}
+
+impl highlevel::Module {
+    pub fn from_file<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        Ok(lowlevel::Module::from_file(path)?.into())
+    }
+
+    pub fn to_file<P: AsRef<Path>>(self, path: P) -> io::Result<usize> {
+        let module: lowlevel::Module = self.into();
+        module.to_file(path)
     }
 }
