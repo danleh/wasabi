9627cd50695ce0bab8129ac40d40516f44af9717
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-02-21 14:17:59 +0100

    add idea for inter-play between host/instrumented binary and callbacks/analysis binary

--- instrumentation/own/wasm-instrumentation/src/instrument.rs
+++ instrumentation/own/wasm-instrumentation/src/instrument.rs
@@ -5,6 +5,24 @@ use ast::{FunctionType, GlobalType};
 use ast::Mutability::*;
 use ast::ValType::*;
 
+// TODO Idea: provide two options of connecting user analysis (i.e., client instrumentation code)
+// with the instrumented binary (i.e., the "host" code + hooks + import of callbacks):
+// A) "dynamic/late binding": instrument host code once, write many analyses as separate WASM
+//    modules. Use the JavaScript/host WASM API to "link" the two together at runtime, i.e.,
+//    export analysis functions as JS functions and provide them as import object to the
+//    instrumented binary.
+//    Possibly suuper slow since we go WASM <-> JS <-> WASM
+// B) "static binding": Still build the two modules seperately. But use "wasm linker" and "wasm
+//    inliner" to optimize the cross-language boundary away.
+//
+//    Step 1 WASM linker: Append all contents from the analysis module onto the host binary. Then
+//    replace all imported functions (in the host binary) with code from the exported functions
+//    of the analysis module IFF their names match.
+//
+//    Step 2 inlining (possibly by external tool, WABT?):
+//    Trivial inlining: if function body is empty (since most callbacks won't be used by the
+//    analysis module), remove the call to the function + setup of function arguments
+
 pub fn identity(_: &mut Module) {}
 
 pub fn add_empty_function(module: &mut Module) {
