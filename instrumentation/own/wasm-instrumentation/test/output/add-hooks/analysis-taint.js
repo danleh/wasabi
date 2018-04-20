3778df02d4a1fac10063e74d8c78f253f3e2f068
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-04-20 15:13:58 +0200

    basic stack emulation test in JavaScript/analysis code: Wasm stack == JS stack (module memory initialization with Data section)

--- instrumentation/own/wasm-instrumentation/test/output/add-hooks/analysis-taint.js
+++ instrumentation/own/wasm-instrumentation/test/output/add-hooks/analysis-taint.js
@@ -3,9 +3,9 @@ Array.prototype.peek = function () {
     return this[this.length - 1];
 };
 
-function check(jsValue, wasmValue) {
-    if (jsValue.toString() !== wasmValue.toString()) {
-        console.log(jsValue, wasmValue);
+function check(op, location, jsValue, wasmValue) {
+    if ((jsValue === undefined ? "undefined" : jsValue).toString() !== wasmValue.toString()) {
+        console.log(op, location, jsValue, wasmValue);
     }
 }
 
@@ -28,39 +28,36 @@ let valueCounter = 0;
 const sourceFnIdx = [];
 const sinkFnIdx = [];
 
-// TODO add push and pop for every instruction
-// TODO correctness: are popped values == on JS replicated stack
-
 /*
  * User-facing API for dynamic analyses.
  */
 
 function if_(location, condition) {
     const jsCondition = values().pop() === 1;
-    if (jsCondition !== condition)
-        console.log(jsCondition, condition);
+    check("if", location, jsCondition, condition);
 }
 
 function br(location, target) {
     const clearThisBlock = stack.peek().blocks.pop();
 }
 
-function br_if(locataion, conditionalTarget, condition) {
+function br_if(location, conditionalTarget, condition) {
     const jsCondition = values().pop() === 1;
-    // console.log(jsCondition, condition);
+    check("br_if", location, jsCondition, condition);
     if (condition) {
         const clearThisBlock = stack.peek().blocks.pop();
     }
 }
 
 function br_table(location, table, defaultTarget, tableIdx) {
-    const clearThisBlock = stack.peek().blocks.pop();
     const jsTableIdx = values().pop();
-    // console.log(jsTableIdx, tableIdx);
+    check("br_table", location, jsTableIdx, tableIdx);
+    const clearThisBlock = stack.peek().blocks.pop();
 }
 
 function begin(location, type) {
     if (type === "function") {
+        // TODO set locals to parameter values of function?
         stack.peek().func = location.func;
     }
     stack.peek().blocks.push([]);
@@ -74,23 +71,24 @@ function end(location, type, beginLocation) {
 }
 
 function drop(location) {
-    values().pop();
+    values().pop(); // FIXME cannot compare since wasm value would need monomorphization, which needs stack typing
 }
 
-function select(location, cond) {
-    values().pop();
-    values().pop();
-    values().pop();
+function select(location, condition) {
+    values().pop(); // FIXME cannot compare since wasm value would need monomorphization, which needs stack typing
+    values().pop(); // FIXME cannot compare since wasm value would need monomorphization, which needs stack typing
+    const jsCondition = values().pop() === 1;
+    check("select", location, jsCondition, condition);
 }
 
 function call_(location, targetFunc, indirect, args) {
-    for (const arg of args) {
-        const jsArg = values().pop();
-        // console.log(jsArg, arg);
-    }
     if (indirect) {
         const jsTargetTableIdx = values().pop();
-        // console.log(jsTargetTableIdx, targetFunc);
+        check("call_indirect table idx", location, jsTargetTableIdx, targetFunc);
+    }
+    for (const arg of args.reverse()) {
+        const jsArg = values().pop();
+        check("call args", location, jsArg, arg);
     }
     // add stack frame
     stack.push({
@@ -118,40 +116,34 @@ function const_(location, value) {
 
 function unary(location, op, input, result) {
     const jsInput = values().pop();
+    check(op, location, jsInput, input);
     values().push(result);
 }
 
 function binary(location, op, first, second, result) {
     const jsSecond = values().pop();
+    check(op + " second arg", location, jsSecond, second);
     const jsFirst = values().pop();
-    if (jsFirst !== first)
-        console.log(jsFirst, first);
-    if (jsSecond !== second)
-        console.log(jsSecond, second);
+    check(op + " first arg", location, jsFirst, first);
     values().push(result);
 }
 
 function load(location, op, memarg, value) {
     const jsAddr = values().pop();
-    if (jsAddr !== memarg.addr) {
-        console.log(location);
-    }
-    const jsValue = memory[memarg.addr + memarg.offset];
-    if (jsValue !== value) {
-        console.log(location, jsValue, value);
-    }
-
+    check(op + " addr", location, jsAddr, memarg.addr);
+    const effectiveAddr = memarg.addr + memarg.offset;
+    const jsValue = memory[effectiveAddr] || 0;
+    // check(op + " value @ " + effectiveAddr + " (0x" + effectiveAddr.toString(16) + ")", location, jsValue, value); // FIXME doesn't work for initialized memory by Data section...
     values().push(value);
 }
 
 function store(location, op, memarg, value) {
     const jsValue = values().pop();
-    if (jsValue.toString() !== value.toString()) {
-        console.log(location);
-    }
+    check(op + " value", location, jsValue, value);
     const jsAddr = values().pop();
-    check(jsAddr, memarg.addr);
-    memory[memarg.addr + memarg.offset] = value;
+    check(op + " addr", location, jsAddr, memarg.addr);
+    const effectiveAddr = memarg.addr + memarg.offset;
+    memory[effectiveAddr] = value;
 }
 
 function current_memory(location, currentSizePages) {
@@ -160,21 +152,20 @@ function current_memory(location, currentSizePages) {
 
 function grow_memory(location, byPages, previousSizePages) {
     let jsByPages = values().pop();
+    check("grow_memory", location, jsByPages, byPages);
     values().push(previousSizePages);
 }
-
-// TODO local/global: uninitialized should return 0, right? i.e. do || 0 for get
 
 function local(location, op, localIndex, value) {
     switch (op) {
         case "set":
             const jsValue = values().pop();
-            // if (jsValue !== value) console.log(location, jsValue, value);
+            check("set_local", location, jsValue, value);
             stack.peek().locals[localIndex] = value;
             return;
         case "tee":
             const jsValue2 = values().peek();
-            if (jsValue2 !== value) console.log(location, jsValue2, value);
+            check("tee_local", location, jsValue2, value);
             stack.peek().locals[localIndex] = value;
             return;
         case "get":
@@ -187,7 +178,7 @@ function global(location, op, globalIndex, value) {
     switch (op) {
         case "set":
             const jsValue = values().pop();
-            if (jsValue !== value) console.log(location, jsValue, value);
+            check("set_global", location, jsValue, value);
             globals[globalIndex] = value;
             return;
         case "get":
