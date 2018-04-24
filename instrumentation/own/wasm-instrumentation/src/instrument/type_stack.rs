72537d45b661c11d4b1ce2ef7eda6731f882883b
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-04-24 15:10:15 +0200

    refactor and simplify TypeStack impl

--- instrumentation/own/wasm-instrumentation/src/instrument/type_stack.rs
+++ instrumentation/own/wasm-instrumentation/src/instrument/type_stack.rs
@@ -2,6 +2,7 @@ use ast::{BlockType, FunctionType, ValType};
 use self::TypeStackElement::*;
 
 /// Abstract Wasm stack
+// TODO handle function begin and end, by adding FunctionType to TypeStack struct
 #[derive(Debug)]
 pub struct TypeStack(Vec<TypeStackElement>);
 
@@ -16,24 +17,26 @@ impl TypeStack {
         TypeStack(Vec::new())
     }
 
-    pub fn topn(&self, n: usize) -> Vec<ValType> {
-        self.0.iter()
-            .rev()
-            .filter_map(|el| match el {
-                Val(ty) => Some(*ty),
-                _ => None
-            })
-            .take(n)
-            .collect()
+    pub fn push(&mut self, ty: ValType) {
+        self.0.push(Val(ty))
     }
 
+    /// panics if stack is empty or if there was a block begin (and not a ValType)
+    pub fn pop(&mut self) -> ValType {
+        match self.0.pop() {
+            None => panic!("tried to pop from empty type stack"),
+            Some(BlockBegin(_)) => panic!("expected ValType on type stack, but got block begin marker indicating empty block stack; full type stack was {:?}", self.0),
+            Some(Val(ty)) => ty
+        }
+    }
+
+    /// convenience, pops and validates input_tys, then pushes the result_tys
     pub fn op(&mut self, input_tys: &[ValType], result_tys: &[ValType]) {
         for &input_ty in input_tys.iter().rev() {
-            // TODO better error message
-            assert_eq!(Val(input_ty), self.0.pop().expect("tried to pop empty abstract stack"), "invalid abstract stack")
+            assert_eq!(input_ty, self.pop());
         }
         for &result_ty in result_tys {
-            self.0.push(Val(result_ty));
+            self.push(result_ty);
         }
     }
 
@@ -41,27 +44,22 @@ impl TypeStack {
         self.0.push(BlockBegin(block_ty))
     }
 
+    /// implicitly pops all types from the stack until the last block begin
+    /// pushes that blocks result type on the stack
+    /// returns the BlockType of that last block
     pub fn end_block(&mut self) -> BlockType {
-        // find last block begin
-        let mut block_begin = None;
-        for (idx, el) in self.0.iter().enumerate().rev() {
-            match el {
-                &BlockBegin(ty) => {
-                    block_begin = Some((idx, ty));
-                    break;
-                }
-                _ => {}
+        loop {
+            match self.0.pop() {
+                None => panic!("tried to end block by popping from type stack until block begin, but no block begin was found"),
+                Some(Val(ty)) => {},
+                Some(BlockBegin(block_ty)) => {
+                    // TODO validate that popped values so far == block type
+                    if let BlockType(Some(ty)) = block_ty {
+                        self.push(ty);
+                    }
+                    return block_ty
+                },
             }
-        }
-
-        if let Some((idx, block_ty)) = block_begin {
-            self.0.truncate(idx);
-            if let BlockType(Some(ty)) = block_ty {
-                self.0.push(Val(ty));
-            }
-            block_ty
-        } else {
-            unreachable!("trying to end a block, but none was begun, abstract stack is {:?}", self);
         }
     }
 }
