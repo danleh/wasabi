6230d4a28ef10fe5e715161d16710f7312b16fbb
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-04-26 16:56:15 +0200

    minor refactoring: rename functions (was confusing: _block is not meant as the instruction, but any type of block), add else_ shorthand

--- instrumentation/own/wasm-instrumentation/src/instrument/type_stack.rs
+++ instrumentation/own/wasm-instrumentation/src/instrument/type_stack.rs
@@ -1,22 +1,26 @@
 use ast::{BlockType, FunctionType, ValType};
 use self::TypeStackElement::*;
 
-#[derive(Debug, new)]
-pub struct TypeStack(#[new(default)] Vec<TypeStackElement>);
+#[derive(Debug)]
+pub struct TypeStack(Vec<TypeStackElement>);
 
 #[derive(Debug, PartialEq)]
-pub enum TypeStackElement {
+enum TypeStackElement {
     Val(ValType),
     BlockBegin(BlockType),
 }
 
 impl TypeStack {
-    pub fn push(&mut self, ty: ValType) {
+    pub fn new() -> Self {
+        TypeStack(vec![])
+    }
+
+    pub fn push_val(&mut self, ty: ValType) {
         self.0.push(Val(ty))
     }
 
     /// panics if stack is empty or if there was a block begin (and not a ValType)
-    pub fn pop(&mut self) -> ValType {
+    pub fn pop_val(&mut self) -> ValType {
         match self.0.pop() {
             None => panic!("tried to pop from empty type stack"),
             Some(BlockBegin(_)) => panic!("expected ValType on type stack, but got block begin marker indicating empty block stack; full type stack was {:?}", self.0),
@@ -25,36 +29,41 @@ impl TypeStack {
     }
 
     /// convenience, pops and validates input_tys, then pushes the result_tys
-    pub fn op(&mut self, input_tys: &[ValType], result_tys: &[ValType]) {
+    pub fn instr(&mut self, input_tys: &[ValType], result_tys: &[ValType]) {
         for &input_ty in input_tys.iter().rev() {
-            assert_eq!(input_ty, self.pop());
+            assert_eq!(input_ty, self.pop_val());
         }
         for &result_ty in result_tys {
-            self.push(result_ty);
+            self.push_val(result_ty);
         }
     }
 
-    pub fn begin_block(&mut self, block_ty: BlockType) {
+    pub fn begin(&mut self, block_ty: BlockType) {
         self.0.push(BlockBegin(block_ty))
     }
 
     /// implicitly pops all types from the stack until the last block begin
     /// pushes that blocks result type on the stack
     /// returns the BlockType of that last block
-    pub fn end_block(&mut self) -> BlockType {
+    pub fn end(&mut self) -> BlockType {
         loop {
             match self.0.pop() {
-                None => panic!("tried to end block by popping from type stack until block begin, but no block begin was found"),
+                None => panic!("could not end block, no block begin was found on type stack"),
                 Some(Val(_ty)) => {},
                 Some(BlockBegin(block_ty)) => {
                     // NOTE there is no validation that the stack is correct at the end of a block
                     // it is unclear to me how it exactly works with, e.g., br/return + drops
                     if let BlockType(Some(ty)) = block_ty {
-                        self.push(ty);
+                        self.push_val(ty);
                     }
                     return block_ty
                 },
             }
         }
+    }
+
+    pub fn else_(&mut self) {
+        let block_ty = self.end();
+        self.begin(block_ty);
     }
 }
