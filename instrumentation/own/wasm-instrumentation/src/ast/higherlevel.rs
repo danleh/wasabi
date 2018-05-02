077f6b8f4fd1ca7ebf0f83da9085074a2aa1f0e0
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-05-02 14:15:06 +0200

    optimization: store types in InstrType as Box<[ValTy]> not Vec<ValTy>

--- instrumentation/own/wasm-instrumentation/src/ast/higherlevel.rs
+++ instrumentation/own/wasm-instrumentation/src/ast/higherlevel.rs
@@ -1,6 +1,6 @@
 use ast::{BlockType, FunctionType, Idx, Label, Local, Memarg, Val, ValType, ValType::*};
 use ast::highlevel::{Function, Global, Memory, Table};
-use self::{LocalOp::*, GlobalOp::*, LoadOp::*, StoreOp::*};
+use self::{GlobalOp::*, LoadOp::*, LocalOp::*, StoreOp::*};
 
 // TODO replace highlevel::Instr with this and InstrGroup with InstrType
 
@@ -229,10 +229,21 @@ enum NumericOp {
 
 /* Type information for each instruction */
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Ord, PartialOrd, Default, new)]
-struct InstrType {
-    inputs: Vec<ValType>,
-    results: Vec<ValType>,
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Ord, PartialOrd, Default)]
+pub struct InstrType {
+    // use Box not Vec (saves the capacity field, i.e., smaller memory size) since InstrType is
+    // almost always immutable anyway (i.e., no dynamic adding/removing of input/result types)
+    inputs: Box<[ValType]>,
+    results: Box<[ValType]>,
+}
+
+impl InstrType {
+    pub fn new(inputs: &[ValType], results: &[ValType]) -> Self {
+        InstrType {
+            inputs: inputs.into(),
+            results: results.into(),
+        }
+    }
 }
 
 impl NumericOp {
@@ -241,45 +252,45 @@ impl NumericOp {
         match *self {
             /* Unary */
 
-            I32Eqz => InstrType::new(vec![I32], vec![I32]),
-            I64Eqz => InstrType::new(vec![I64], vec![I32]),
+            I32Eqz => InstrType::new(&[I32], &[I32]),
+            I64Eqz => InstrType::new(&[I64], &[I32]),
 
-            I32Clz | I32Ctz | I32Popcnt => InstrType::new(vec![I32], vec![I32]),
-            I64Clz | I64Ctz | I64Popcnt => InstrType::new(vec![I64], vec![I64]),
+            I32Clz | I32Ctz | I32Popcnt => InstrType::new(&[I32], &[I32]),
+            I64Clz | I64Ctz | I64Popcnt => InstrType::new(&[I64], &[I64]),
 
-            F32Abs | F32Neg | F32Ceil | F32Floor | F32Trunc | F32Nearest | F32Sqrt => InstrType::new(vec![F32], vec![F32]),
-            F64Abs | F64Neg | F64Ceil | F64Floor | F64Trunc | F64Nearest | F64Sqrt => InstrType::new(vec![F64], vec![F64]),
+            F32Abs | F32Neg | F32Ceil | F32Floor | F32Trunc | F32Nearest | F32Sqrt => InstrType::new(&[F32], &[F32]),
+            F64Abs | F64Neg | F64Ceil | F64Floor | F64Trunc | F64Nearest | F64Sqrt => InstrType::new(&[F64], &[F64]),
 
             // conversions
-            I32WrapI64 => InstrType::new(vec![I64], vec![I32]),
-            I32TruncSF32 | I32TruncUF32 => InstrType::new(vec![I32], vec![F32]),
-            I32TruncSF64 | I32TruncUF64 => InstrType::new(vec![F64], vec![I32]),
-            I64ExtendSI32 | I64ExtendUI32 => InstrType::new(vec![I32], vec![I64]),
-            I64TruncSF32 | I64TruncUF32 => InstrType::new(vec![F32], vec![I64]),
-            I64TruncSF64 | I64TruncUF64 => InstrType::new(vec![F64], vec![I64]),
-            F32ConvertSI32 | F32ConvertUI32 => InstrType::new(vec![I32], vec![F32]),
-            F32ConvertSI64 | F32ConvertUI64 => InstrType::new(vec![I64], vec![F32]),
-            F32DemoteF64 => InstrType::new(vec![F64], vec![F32]),
-            F64ConvertSI32 | F64ConvertUI32 => InstrType::new(vec![I32], vec![F64]),
-            F64ConvertSI64 | F64ConvertUI64 => InstrType::new(vec![I64], vec![F64]),
-            F64PromoteF32 => InstrType::new(vec![F32], vec![F64]),
-            I32ReinterpretF32 => InstrType::new(vec![F32], vec![I32]),
-            I64ReinterpretF64 => InstrType::new(vec![F64], vec![I64]),
-            F32ReinterpretI32 => InstrType::new(vec![I32], vec![F32]),
-            F64ReinterpretI64 => InstrType::new(vec![I64], vec![F64]),
+            I32WrapI64 => InstrType::new(&[I64], &[I32]),
+            I32TruncSF32 | I32TruncUF32 => InstrType::new(&[I32], &[F32]),
+            I32TruncSF64 | I32TruncUF64 => InstrType::new(&[F64], &[I32]),
+            I64ExtendSI32 | I64ExtendUI32 => InstrType::new(&[I32], &[I64]),
+            I64TruncSF32 | I64TruncUF32 => InstrType::new(&[F32], &[I64]),
+            I64TruncSF64 | I64TruncUF64 => InstrType::new(&[F64], &[I64]),
+            F32ConvertSI32 | F32ConvertUI32 => InstrType::new(&[I32], &[F32]),
+            F32ConvertSI64 | F32ConvertUI64 => InstrType::new(&[I64], &[F32]),
+            F32DemoteF64 => InstrType::new(&[F64], &[F32]),
+            F64ConvertSI32 | F64ConvertUI32 => InstrType::new(&[I32], &[F64]),
+            F64ConvertSI64 | F64ConvertUI64 => InstrType::new(&[I64], &[F64]),
+            F64PromoteF32 => InstrType::new(&[F32], &[F64]),
+            I32ReinterpretF32 => InstrType::new(&[F32], &[I32]),
+            I64ReinterpretF64 => InstrType::new(&[F64], &[I64]),
+            F32ReinterpretI32 => InstrType::new(&[I32], &[F32]),
+            F64ReinterpretI64 => InstrType::new(&[I64], &[F64]),
 
             /* Binary */
 
-            I32Eq | I32Ne | I32LtS | I32LtU | I32GtS | I32GtU | I32LeS | I32LeU | I32GeS | I32GeU => InstrType::new(vec![I32, I32], vec![I32]),
-            I64Eq | I64Ne | I64LtS | I64LtU | I64GtS | I64GtU | I64LeS | I64LeU | I64GeS | I64GeU => InstrType::new(vec![I64, I64], vec![I32]),
+            I32Eq | I32Ne | I32LtS | I32LtU | I32GtS | I32GtU | I32LeS | I32LeU | I32GeS | I32GeU => InstrType::new(&[I32, I32], &[I32]),
+            I64Eq | I64Ne | I64LtS | I64LtU | I64GtS | I64GtU | I64LeS | I64LeU | I64GeS | I64GeU => InstrType::new(&[I64, I64], &[I32]),
 
-            F32Eq | F32Ne | F32Lt | F32Gt | F32Le | F32Ge => InstrType::new(vec![F32, F32], vec![I32]),
-            F64Eq | F64Ne | F64Lt | F64Gt | F64Le | F64Ge => InstrType::new(vec![F64, F64], vec![I32]),
+            F32Eq | F32Ne | F32Lt | F32Gt | F32Le | F32Ge => InstrType::new(&[F32, F32], &[I32]),
+            F64Eq | F64Ne | F64Lt | F64Gt | F64Le | F64Ge => InstrType::new(&[F64, F64], &[I32]),
 
-            I32Add | I32Sub | I32Mul | I32DivS | I32DivU | I32RemS | I32RemU | I32And | I32Or | I32Xor | I32Shl | I32ShrS | I32ShrU | I32Rotl | I32Rotr => InstrType::new(vec![I32, I32], vec![I32]),
-            I64Add | I64Sub | I64Mul | I64DivS | I64DivU | I64RemS | I64RemU | I64And | I64Or | I64Xor | I64Shl | I64ShrS | I64ShrU | I64Rotl | I64Rotr => InstrType::new(vec![I64, I64], vec![I64]),
-            F32Add | F32Sub | F32Mul | F32Div | F32Min | F32Max | F32Copysign => InstrType::new(vec![F32, F32], vec![F32]),
-            F64Add | F64Sub | F64Mul | F64Div | F64Min | F64Max | F64Copysign => InstrType::new(vec![F64, F64], vec![F64]),
+            I32Add | I32Sub | I32Mul | I32DivS | I32DivU | I32RemS | I32RemU | I32And | I32Or | I32Xor | I32Shl | I32ShrS | I32ShrU | I32Rotl | I32Rotr => InstrType::new(&[I32, I32], &[I32]),
+            I64Add | I64Sub | I64Mul | I64DivS | I64DivU | I64RemS | I64RemU | I64And | I64Or | I64Xor | I64Shl | I64ShrS | I64ShrU | I64Rotl | I64Rotr => InstrType::new(&[I64, I64], &[I64]),
+            F32Add | F32Sub | F32Mul | F32Div | F32Min | F32Max | F32Copysign => InstrType::new(&[F32, F32], &[F32]),
+            F64Add | F64Sub | F64Mul | F64Div | F64Min | F64Max | F64Copysign => InstrType::new(&[F64, F64], &[F64]),
         }
     }
 }
@@ -287,21 +298,21 @@ impl NumericOp {
 impl LoadOp {
     fn to_type(&self) -> InstrType {
         match *self {
-            I32Load => InstrType::new(vec![], vec![I32]),
-            I64Load => InstrType::new(vec![], vec![I64]),
-            F32Load => InstrType::new(vec![], vec![F32]),
-            F64Load => InstrType::new(vec![], vec![F64]),
+            I32Load => InstrType::new(&[], &[I32]),
+            I64Load => InstrType::new(&[], &[I64]),
+            F32Load => InstrType::new(&[], &[F32]),
+            F64Load => InstrType::new(&[], &[F64]),
 
-            I32Load8S => InstrType::new(vec![], vec![I32]),
-            I32Load8U => InstrType::new(vec![], vec![I32]),
-            I32Load16S => InstrType::new(vec![], vec![I32]),
-            I32Load16U => InstrType::new(vec![], vec![I32]),
-            I64Load8S => InstrType::new(vec![], vec![I64]),
-            I64Load8U => InstrType::new(vec![], vec![I64]),
-            I64Load16S => InstrType::new(vec![], vec![I64]),
-            I64Load16U => InstrType::new(vec![], vec![I64]),
-            I64Load32S => InstrType::new(vec![], vec![I64]),
-            I64Load32U => InstrType::new(vec![], vec![I64]),
+            I32Load8S => InstrType::new(&[], &[I32]),
+            I32Load8U => InstrType::new(&[], &[I32]),
+            I32Load16S => InstrType::new(&[], &[I32]),
+            I32Load16U => InstrType::new(&[], &[I32]),
+            I64Load8S => InstrType::new(&[], &[I64]),
+            I64Load8U => InstrType::new(&[], &[I64]),
+            I64Load16S => InstrType::new(&[], &[I64]),
+            I64Load16U => InstrType::new(&[], &[I64]),
+            I64Load32S => InstrType::new(&[], &[I64]),
+            I64Load32U => InstrType::new(&[], &[I64]),
         }
     }
 }
@@ -309,16 +320,16 @@ impl LoadOp {
 impl StoreOp {
     fn to_type(&self) -> InstrType {
         match *self {
-            I32Store => InstrType::new(vec![I32], vec![]),
-            I64Store => InstrType::new(vec![I64], vec![]),
-            F32Store => InstrType::new(vec![F32], vec![]),
-            F64Store => InstrType::new(vec![F64], vec![]),
+            I32Store => InstrType::new(&[I32], &[]),
+            I64Store => InstrType::new(&[I64], &[]),
+            F32Store => InstrType::new(&[F32], &[]),
+            F64Store => InstrType::new(&[F64], &[]),
 
-            I32Store8 => InstrType::new(vec![I32], vec![]),
-            I32Store16 => InstrType::new(vec![I32], vec![]),
-            I64Store8 => InstrType::new(vec![I64], vec![]),
-            I64Store16 => InstrType::new(vec![I64], vec![]),
-            I64Store32 => InstrType::new(vec![I64], vec![]),
+            I32Store8 => InstrType::new(&[I32], &[]),
+            I32Store16 => InstrType::new(&[I32], &[]),
+            I64Store8 => InstrType::new(&[I64], &[]),
+            I64Store16 => InstrType::new(&[I64], &[]),
+            I64Store32 => InstrType::new(&[I64], &[]),
         }
     }
 }
@@ -332,9 +343,9 @@ impl Instr {
             Unreachable | Nop => Some(InstrType::default()),
             Load(ref op, _) => Some(op.to_type()),
             Store(ref op, _) => Some(op.to_type()),
-            MemorySize(_) => Some(InstrType::new(vec![], vec![I32])),
-            MemoryGrow(_) => Some(InstrType::new(vec![I32], vec![I32])),
-            Const(ref val) => Some(InstrType::new(vec![], vec![val.to_type()])),
+            MemorySize(_) => Some(InstrType::new(&[], &[I32])),
+            MemoryGrow(_) => Some(InstrType::new(&[I32], &[I32])),
+            Const(ref val) => Some(InstrType::new(&[], &[val.to_type()])),
             Numeric(ref op) => Some(op.to_type()),
 
             // nesting...
