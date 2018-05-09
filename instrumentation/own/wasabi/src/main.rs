2fed23dda5a108c581995310fe0c40a0be51704a
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-05-09 17:30:04 +0200

    add wasabi binary with simple command line args (first: program to instrument, second: output dir)

--- instrumentation/own/wasabi/src/main.rs
+++ instrumentation/own/wasabi/src/main.rs
@@ -1,3 +1,44 @@
+extern crate wasabi;
+
+use std::{env, fs, path::PathBuf, io};
+use wasabi::{wasm::ast::highlevel::Module, instrument::add_hooks};
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(error) = main_inner() {
+        eprintln!(r#"Error: {}
+
+Usage: wasabi <input_wasm_file> [<output_dir>]
+
+Produces two files in <output_dir> (default: out/):
+  - an instrumented version of the <input_wasm_file> and
+  - a JavaScript file with static analysis information and (Wasabi internal) low-level hooks."#,
+                  error);
+    }
+}
+
+fn main_inner() -> io::Result<()> {
+    // skip first argument (program name)
+    let mut args = env::args().skip(1);
+    let input_file = PathBuf::from(args.next().ok_or(io_err("expected at least one argument"))?);
+    let output_dir = PathBuf::from(args.next().unwrap_or("out".to_string()));
+
+    let input_filename_no_ext = input_file.file_stem().ok_or(io_err("invalid input file"))?;
+
+    let mut output_file_stem = output_dir.clone();
+    output_file_stem.push(input_filename_no_ext);
+    let output_file_wasm = output_file_stem.with_extension("wasm");
+    let output_file_js = output_file_stem.with_extension("js");
+
+    // instrument Wasm and generate JavaScript
+    let mut module = Module::from_file(input_file.clone())?;
+    let js = add_hooks(&mut module).unwrap();
+
+    // write output files
+    fs::create_dir_all(output_dir)?;
+    module.to_file(output_file_wasm)?;
+    fs::write(output_file_js, js)
+}
+
+fn io_err(str: &str) -> io::Error {
+    io::Error::new(io::ErrorKind::InvalidInput, str.to_string())
 }
