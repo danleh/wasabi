13bd3a5252042016b4bb5c0d8e7672e956e39173
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-07-18 18:11:44 +0200

    compare not against --hooks="none" but against the original files, exclude generated JavaScript from code size, do not write file (script user can do that), exclude grouped/ root dir

--- bench/4-instrumented-code-size.sh
+++ bench/4-instrumented-code-size.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-(
 echo "bytes \t overhead \t hooks"
-none_size=$(du --apparent-size wasm/instrumented/none | cut -f 1)
-du --apparent-size wasm/instrumented/* | sort | while read line
+original_size=$(du --apparent-size wasm/original | cut -f 1)
+echo "$original_size \t 0% \t <original>"
+du -S --apparent-size --exclude=*.wasabi.js wasm/instrumented/* | sort -V | grep -v 'grouped$' | while read line
 do
 	size=$(echo $line | cut -d' ' -f 1)
 	hooks=$(basename $(echo $line | cut -d' ' -f 2))
-	overhead=$(echo "scale=5;(($size/$none_size)-1)*100" | bc)
-	echo "$size \t $overhead%     \t $hooks"
+	overhead=$(echo "scale=5;(($size/$original_size)-1)*100" | bc)
+	echo "$size \t $overhead% \t $hooks"
 done
-) > 4_results
