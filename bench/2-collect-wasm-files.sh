13ec657746b430f8907c10dc54d3778e890045f7
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-07-18 15:45:01 +0200

    normal sh suffices, no need for bash; do not make wasm/instrumented/* dirs, let later stage handle that

--- bench/2-collect-wasm-files.sh
+++ bench/2-collect-wasm-files.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-mkdir -p wasm/original wasm/instrumented
+#!/bin/sh
+mkdir -p wasm/original/
 cp programs/polybench-c-4.2.1-beta/build/*.wasm wasm/original/
 cp programs/pspdfkit-webassembly-benchmark-master/public/vendor/pspdfkit/pspdfkit-lib/pspdfkit.wasm wasm/original/
 gzip -c -d programs/EpicZenGarden/2017-03-16-ZenGarden/UE4Game-HTML5-Shipping.wasm.gz > wasm/original/UE4Game-HTML5-Shipping.wasm
