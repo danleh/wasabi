18c53536cca08f8df3b69e0c33c528fdd76cca12
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-07-19 14:43:06 +0200

    add script that copies wasm, wasabi.js, and analysis.js files to appropriate locations

--- eval/5-prepare-programs-for-analysis.sh
+++ eval/5-prepare-programs-for-analysis.sh
@@ -1,7 +1,50 @@
 #!/bin/bash
 
-# copy set-up programs before modifying them for the analysis
-mkdir -p programs-analysis/polybench-c-4.2.1-beta/
-cp -r programs/EpicZenGarden/ programs-analysis/
-cp -r programs/pspdfkit-webassembly-benchmark-master/ programs-analysis/
-cp programs/polybench-c-4.2.1-beta/build/* programs-analysis/polybench-c-4.2.1-beta/
+# copy programs before modifying them for the analyses
+if [ ! -d programs-analysis/ ]
+then
+	echo "programs-analysis/ directory not found, copying from programs/ and inserting Wasabi scripts in html"
+	mkdir -p programs-analysis/polybench-c-4.2.1-beta/
+	cp -r programs/EpicZenGarden/ programs-analysis/
+	cp -r programs/pspdfkit-webassembly-benchmark-master/ programs-analysis/
+	cp programs/polybench-c-4.2.1-beta/build/* programs-analysis/polybench-c-4.2.1-beta/
+
+	# insert *.wasabi.js and analysis.js scripts into html harnesses
+	sed -i "4i\    <script src=\"UE4Game-HTML5-Shipping.wasabi.js\"></script><script src=\"analysis.js\"></script>" programs-analysis/EpicZenGarden/2017-03-16-ZenGarden/EpicZenGarden.html
+	sed -i "4i\    <script src=\"pspdfkit.wasabi.js\"></script><script src=\"analysis.js\"></script>" programs-analysis/pspdfkit-webassembly-benchmark-master/public/index.html
+	for file in programs-analysis/polybench-c-4.2.1-beta/*.html
+	do
+		name=$(basename $file .html)
+		sed -i "4i\    <script src=\"$name.wasabi.js\"></script><script src=\"analysis.js\"></script>" $file
+	done
+fi
+
+analysis=$1
+hooks=$2
+
+if [ -z $analysis ] || [ -z $hooks ]
+then
+	echo "usage:"
+	echo "  $0 <analysis> <hooks>"
+	echo "where:"
+	echo "  <analysis> file from ../analyses/*, without .js extension"
+	echo "  <hooks>    subdirectory from ./wasm/instrumented/*"
+	exit
+fi
+
+# copy instrumented *.wasm file, generated *.wasabi.js file, and analysis into harness directories
+gzip --stdout --fast wasm/instrumented/$hooks/UE4Game-HTML5-Shipping.wasm > programs-analysis/EpicZenGarden/2017-03-16-ZenGarden/UE4Game-HTML5-Shipping.wasm.gz
+cp wasm/instrumented/$hooks/UE4Game-HTML5-Shipping.wasabi.js programs-analysis/EpicZenGarden/2017-03-16-ZenGarden/
+
+cp wasm/instrumented/$hooks/pspdfkit.wasabi.js programs-analysis/pspdfkit-webassembly-benchmark-master/public/
+cp wasm/instrumented/$hooks/pspdfkit.wasm programs-analysis/pspdfkit-webassembly-benchmark-master/public/vendor/pspdfkit/pspdfkit-lib/
+
+find wasm/instrumented/$hooks/ -type f ! -iname '*UE4*' -and ! -iname '*pspdf*' -exec cp -t programs-analysis/polybench-c-4.2.1-beta/ {} +
+
+cp ../analyses/$analysis.js programs-analysis/EpicZenGarden/2017-03-16-ZenGarden/analysis.js
+cp ../analyses/$analysis.js programs-analysis/pspdfkit-webassembly-benchmark-master/public/analysis.js
+cp ../analyses/$analysis.js programs-analysis/polybench-c-4.2.1-beta/analysis.js
+
+#emrun --browser ~/Downloads/nightly\ browsers\ as\ of\ 2018-07-11\ 13-30\ UTC+0/chrome-linux/chrome out/atax.html 
+#emrun --no_browser --port=8080 out/atax.html &
+#~/Downloads/nightly\ browsers\ as\ of\ 2018-07-11\ 13-30\ UTC+0/firefox-63.0a1.de.linux-x86_64/firefox/firefox -no-remote -new-instance -profile ./ffprofile/ http://localhost:8080/atax.html
