214548a1100960fc0acd2b91f52c8674dde6761f
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-07-19 11:22:51 +0200

    clarify that size overhead measures only wasm files (not generated JS)

--- bench/4-instrumented-code-size.sh
+++ bench/4-instrumented-code-size.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-echo "bytes \t overhead \t hooks"
+echo "bytes \t wasm overhead \t hooks"
 original_size=$(du --apparent-size wasm/original | cut -f 1)
 echo "$original_size \t 0% \t <original>"
-du -S --apparent-size --exclude=*.wasabi.js wasm/instrumented/* | sort -V | grep -v 'grouped$' | while read line
+du -S --exclude=*.wasabi.js --apparent-size wasm/instrumented/* | grep -v 'grouped$' | while read line
 do
 	size=$(echo $line | cut -d' ' -f 1)
 	hooks=$(basename $(echo $line | cut -d' ' -f 2))
 	overhead=$(echo "scale=5;(($size/$original_size)-1)*100" | bc)
 	echo "$size \t $overhead% \t $hooks"
-done
+done | sort -V
