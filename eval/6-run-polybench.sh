2edddd0878f0d4a6cc20ae6cf26ae9cd45c8a6ab
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-07-23 12:07:26 +0200

    start emrun once per test, also automatically kill browser after test completed

--- eval/6-run-polybench.sh
+++ eval/6-run-polybench.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-firefox="$(readlink -f browsers/firefox-63-nightly/firefox) -no-remote -profile $(readlink -f browsers/firefox-profile)"
-chrome="$(readlink -f browsers/chromium-69-nightly/chrome) --user-data-dir=$(readlink -f browsers/chromium-profile)"
+firefox_bin="$(readlink -f browsers/firefox-63-nightly/firefox)"
+firefox_args="-no-remote -profile $(readlink -f browsers/firefox-profile)"
 
-echo "$firefox"
-echo "$chrome"
+chrome_bin="$(readlink -f browsers/chromium-69-nightly/chrome)"
+chrome_args="--user-data-dir=$(readlink -f browsers/chromium-profile)"
 
 emrun_output="$(readlink -f emrun_output)"
+
+analysis="$1"
 
 # ensure emcc is on PATH, e.g., by running 
 source ~/Documents/SOLA/WebAssembly/tools/emsdk/emsdk_env.sh
 
-emrun --no_browser --port 6337 --log_stdout $emrun_output --serve_after_exit --serve_root programs-analysis/polybench-c-4.2.1-beta . &
-emrun_pid=$!
+# break out of loop on Ctrl+C
+trap exit SIGINT SIGTERM
 
-for name in programs-analysis/polybench-c-4.2.1-beta/*.html
+for file in programs-analysis/polybench-c-4.2.1-beta/*.html
 do
-	name=$(basename $name .html)
-	echo -n "$name;" >> $emrun_output
-	$firefox http://localhost:6337/$name.html
+	name=$(basename $file .html)
+	echo -n "$name;$analysis;" >> $emrun_output
+	emrun --log_stdout "$emrun_output" --browser "$firefox_bin" --browser_args "$firefox_args" --kill_exit "$file"
 done
-
-kill $emrun_pid
