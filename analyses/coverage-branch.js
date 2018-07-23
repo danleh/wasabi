33b61f235871af7a02bec04b6e1a41569edcc759
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-07-23 16:22:21 +0200

    simplify (remove result printing to console), make consistent with other analyses (i.e. Wasabi.analysisResult contains result), code style: move into block as to not pollute global namespace

--- analyses/coverage-branch.js
+++ analyses/coverage-branch.js
@@ -1,42 +1,33 @@
-const coverageData = [];
+{
+    const coverageData = [];
+    Wasabi.analysisResult = coverageData;
 
-// branch can be boolean (for if and br_if) or integer (for br_table, i.e., switches)
-function addBranch(location, branch) {
-    if (coverageData[location.func] === undefined) {
-        coverageData[location.func] = [];
-    }
-    if (coverageData[location.func][location.instr] === undefined) {
-        coverageData[location.func][location.instr] = new Set();
-    }
-    coverageData[location.func][location.instr].add(branch);
-}
-
-// call after the program-to-analyze has completed
-function results() {
-    for (const [fnIdx, fnCov] of coverageData.entries()) {
-        if (fnCov !== undefined) {
-            for (const [instrIdx, instrCov] of fnCov.entries()) {
-                if (instrCov !== undefined)
-                    console.log("function", fnIdx, "instruction", instrIdx, "branches covered:", [...instrCov])
-            }
+    // branch can be boolean (for if and br_if) or integer (for br_table, i.e., switches)
+    function addBranch(location, branch) {
+        if (coverageData[location.func] === undefined) {
+            coverageData[location.func] = [];
         }
+        if (coverageData[location.func][location.instr] === undefined) {
+            coverageData[location.func][location.instr] = new Set();
+        }
+        coverageData[location.func][location.instr].add(branch);
     }
+
+    Wasabi.analysis = {
+        if_(location, condition) {
+            addBranch(location, condition);
+        },
+
+        br_if(location, conditionalTarget, condition) {
+            addBranch(location, condition);
+        },
+
+        br_table(location, table, defaultTarget, tableIdx) {
+            addBranch(location, tableIdx);
+        },
+
+        select(location, condition) {
+            addBranch(location, condition);
+        },
+    };
 }
-
-Wasabi.analysis = {
-    if_(location, condition) {
-        addBranch(location, condition);
-    },
-
-    br_if(location, conditionalTarget, condition) {
-        addBranch(location, condition);
-    },
-
-    br_table(location, table, defaultTarget, tableIdx) {
-        addBranch(location, tableIdx);
-    },
-
-    select(location, condition) {
-        addBranch(location, condition);
-    },
-};
--- analyses/coverage-instruction.js
+++ analyses/coverage-instruction.js
@@ -1,33 +1,21 @@
-const coverageData = [];
+{
+    const coverageData = [];
+    Wasabi.analysisResult = coverageData;
 
-function addLocation(location) {
-    if (coverageData[location.func] === undefined) {
-        coverageData[location.func] = new Set();
-    }
-    // ignore "virtual instructions" that are not actually present in the binary
-    // (e.g., "begin_function" hook or implicit returns)
-    // for them location.instr === -1
-    if (location.instr >= 0) {
-        coverageData[location.func].add(location.instr);
-    }
-}
-
-// call after the program-to-analyze has completed
-function results() {
-    for (const [fnIdx, fn] of Wasabi.module.info.functions.entries()) {
-        // imported functions can naturally not have coverage information
-        if (fn.import !== null) {
-            continue;
+    function addLocation(location) {
+        if (coverageData[location.func] === undefined) {
+            coverageData[location.func] = new Set();
         }
-        console.log("function #", fnIdx, "exported as:", fn.export);
-        const instrs = coverageData[fnIdx] || new Set();
-        console.log("  executed instructions:", instrs.size, ",", [...instrs].sort((a, b) => a - b));
-        console.log("  of a total of:", fn.instrCount);
-        console.log("  coverage (%):", instrs.size / fn.instrCount * 100);
+        // ignore "virtual instructions" that are not actually present in the binary
+        // (e.g., "begin_function" hook or implicit returns)
+        // for them location.instr === -1
+        if (location.instr >= 0) {
+            coverageData[location.func].add(location.instr);
+        }
     }
-}
 
-// just let every callback add its location to coverageData
-for (const hook of Wasabi.HOOK_NAMES) {
-    Wasabi.analysis[hook] = addLocation;
+    // just let every callback add its location to coverageData
+    for (const hook of Wasabi.HOOK_NAMES) {
+        Wasabi.analysis[hook] = addLocation;
+    }
 }
