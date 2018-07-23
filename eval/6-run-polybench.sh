b168b7458513496d94bc2863e658d6b4ae57c590
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-07-23 12:52:29 +0200

    update results format

--- eval/6-run-polybench.sh
+++ eval/6-run-polybench.sh
@@ -6,7 +6,7 @@ firefox_args="-no-remote -profile $(readlink -f browsers/firefox-profile)"
 chrome_bin="$(readlink -f browsers/chromium-69-nightly/chrome)"
 chrome_args="--user-data-dir=$(readlink -f browsers/chromium-profile)"
 
-emrun_output="$(readlink -f emrun_output)"
+emrun_output="$(readlink -f 6_results)"
 
 analysis="$1"
 
@@ -19,6 +19,6 @@ trap exit SIGINT SIGTERM
 for file in programs-analysis/polybench-c-4.2.1-beta/*.html
 do
 	name=$(basename $file .html)
-	echo -n "$name;$analysis;" >> $emrun_output
+	echo -n "firefox;$analysis;$name;" >> $emrun_output
 	emrun --log_stdout "$emrun_output" --browser "$firefox_bin" --browser_args "$firefox_args" --kill_exit "$file"
 done
