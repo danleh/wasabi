001c838b60ef4a759af6d6128600d13096b558f3
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-07-24 13:52:30 +0200

    add cryptominer analysis skeleton (empty with a TODO)

---
+++ analyses/cryptominer-detection.js
@@ -0,0 +1,4 @@
+// see SEISMIC: SEcure In-lined Script Monitors forInterrupting Cryptojacks
+// https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwjHt8KE0rfcAhWMalAKHY75Bl4QFggoMAA&url=http%3A%2F%2Fwww.utdallas.edu%2F~hamlen%2Fwang18esorics.pdf&usg=AOvVaw2u-ajqPIRKG1B7beQ5fVvL
+
+// TODO
