5dc8e869b7b3b3ca5a79c42d6febc33fe5152889
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-07-26 15:23:53 +0200

    add memory tracing and block counting analyses skeletons (TODO)

---
+++ analyses/block-counting.js
@@ -0,0 +1,3 @@
+// TODO
+// only begin hook
+// save execution cound in Map<location, count> and Map<location, block type>
---
+++ analyses/memory-tracing.js
@@ -0,0 +1,5 @@
+// TODO
+// online + offline phase
+// only load and store hooks
+// for every access, save in an array of [{location, address, flag: read/write}]
+// possibly compress this for memory reasons
