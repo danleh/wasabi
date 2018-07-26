e85c3030e962bce6efd75d8ef0a2d94910cf3290
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-07-26 18:41:31 +0200

    add very simple memory trace analysis

--- analyses/memory-tracing.js
+++ analyses/memory-tracing.js
@@ -1,5 +1,16 @@
-// TODO
 // online + offline phase
-// only load and store hooks
-// for every access, save in an array of [{location, address, flag: read/write}]
-// possibly compress this for memory reasons
+{
+	// TODO possibly compress accesses to avoid OOM
+	const accesses /* : [{func, instr, addr, write: bool}] */ = [];
+
+	function access({func, instr}, {addr, offset}, write) {
+		accesses.push({func, instr, addr: addr+offset, write});
+	};
+
+	Wasabi.analysis = {
+		load(loc, op, memarg) { access(loc, memarg, false) },
+		store(loc, op, memarg) { access(loc, memarg, true) },
+	};
+
+	Wasabi.analysisResult = accesses;
+}
