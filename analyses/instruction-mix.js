937d5af674614f01f6ae21c3ddae35963c1fadcc
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-07-27 17:38:16 +0200

    does adding function arguments to the hooks, such that the signatures match to the calls, get us a speedup (because of JIT etc)?

--- analyses/instruction-mix.js
+++ analyses/instruction-mix.js
@@ -9,43 +9,43 @@
     };
 
     function incInstr(instr) {
-        counts[instr] = (counts[instr] || 0) + 1;
+        counts[instr] = (counts[instr] | 0) + 1;
     }
 
     Wasabi.analysis = {
         // hooks that directly correspond to one instruction
-        nop() { incInstr("nop") },
-        unreachable() { incInstr("unreachable") },
-        if_() { incInstr("if") },
-        br() { incInstr("br") },
-        br_if() { incInstr("br_if") },
-        br_table() { incInstr("br_table") },
-        drop() { incInstr("drop") },
-        select() { incInstr("select") },
-        memory_size() { incInstr("memory_size") },
-        memory_grow() { incInstr("memory_grow") },
+        nop(loc) { incInstr("nop") },
+        unreachable(loc) { incInstr("unreachable") },
+        if_(loc, cond) { incInstr("if") },
+        br(loc, target) { incInstr("br") },
+        br_if(loc, target, cond) { incInstr("br_if") },
+        br_table(loc, table, def, idx) { incInstr("br_table") },
+        drop(loc, val) { incInstr("drop") },
+        select(loc, fst, snd, cond) { incInstr("select") },
+        memory_size(loc, val) { incInstr("memory_size") },
+        memory_grow(loc, delta, old) { incInstr("memory_grow") },
 
         // hooks that correspond to multiple instructions -> use op argument
-        unary(loc, op) { incInstr(op) },
-        binary(loc, op) { incInstr(op) },
-        load(loc, op) { incInstr(op) },
-        store(loc, op) { incInstr(op) },
-        local(loc, op) { incInstr(op) },
-        global(loc, op) { incInstr(op) },
+        unary(loc, op, input, result) { incInstr(op) },
+        binary(loc, op, first, second, result) { incInstr(op) },
+        load(loc, op, memarg, val) { incInstr(op) },
+        store(loc, op, memarg, val) { incInstr(op) },
+        local(loc, op, idx, val) { incInstr(op) },
+        global(loc, op, idx, val) { incInstr(op) },
 
         // special cases
         call_pre(loc, func, args, tableIdx) {
             incInstr((tableIdx === undefined) ? "call" : "call_indirect");
         },
-        const_() {
+        const_(loc, val) {
             // FIXME cannot determine const type, so record just as T.const
             incInstr("T.const");
         },
-        begin(loc, type) {
+        begin(loc, type, end) {
             // if is already counted by if_ hook, function begin is implicit
             if (type !== "if" && type !== "function") incInstr(type);
         },
-        return_({func, instr}) {
+        return_({func, instr}, vals) {
             // do not count implicit returns
             if (instr >= 0) incInstr("return");
         },
