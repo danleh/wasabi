d2dd9391ac80e70a24825e098a1ce62403392b70
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-07-30 11:52:45 +0200

    add results seq. vs. par. instrumentation

--- eval/7-runtime-wasabi.sh
+++ eval/7-runtime-wasabi.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
+cargo install --path .. --force 1>&2
 echo "file, time [ms]"
-for i in $(seq 1 10)
+for i in $(seq 1 20)
 do
 	for file in wasm/original/*.wasm
 	do
 		echo -n "$(basename $file), "
 		t_start=$(date +%s%N)
-		cargo run -q --release -- $file
+		# sequential instrumentation
+		# (RAYON_NUM_THREADS=1 wasabi $file)
+		# parallel
+		wasabi $file
+		# write results to /dev/null
+		# wasabi $file /dev/null/out
 		t_ms=$((($(date +%s%N) - $t_start)/1000000))
 		echo "$t_ms"
 	done
