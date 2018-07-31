05381f3088c766850c8b5fbf4b2988ad901bc3c1
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-07-31 09:56:20 +0200

    code size figure works

--- eval/results/code-size-figure.py
+++ eval/results/code-size-figure.py
@@ -5,14 +5,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-print pd.__version__
+# print pd.__version__
 
-data = pd.read_csv("code-size.csv", skipinitialspace=True)
-data = data.drop(["path"], axis=1)
-data["overhead"] = np.where()
-print data.dtypes
-fig, ax = plt.subplots(figsize=(10,4))
-plt.xticks(rotation=45)
-sns.factorplot(x='filename', y='bytes', hue='hooks', kind='bar', data=data, ax=ax)
-fig.savefig("code-size.svg")
-plt.close(fig)
+df = pd.read_csv("code-size.csv", skipinitialspace=True)
+
+df = df.drop(["path"], axis=1)
+df = df[df.hooks != "all"]
+df = df[df.hooks != "instruction-mix"]
+df = df[df.hooks != "coverage-branch"]
+
+for i, row in df.iterrows():
+	original_bytes = df[(df["hooks"] == "original") & (df["filename"] == row["filename"])].iloc[0]["bytes"]
+	df.ix[i, "overhead"] = row["bytes"] / original_bytes
+
+df = df[df.hooks != "original"]
+
+# print df
+# print df.dtypes
+
+# fig, ax = plt.subplots(figsize=(10,4))
+g = sns.factorplot(x='filename', y='overhead', hue='hooks', kind='bar', aspect=3, data=df)
+g.set_xticklabels(rotation=90)
+g.savefig("code-size.svg")
+# plt.close(fig)
