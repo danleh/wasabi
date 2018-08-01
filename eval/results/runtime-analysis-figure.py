38f2254358cae864a16beee8fcf4a54a55706f4f
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-08-01 15:12:44 +0200

    add many different figures for runtime results of polybench programs

--- eval/results/runtime-analysis-figure.py
+++ eval/results/runtime-analysis-figure.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
+import scipy as sp
 
 mpl.font_manager.USE_FONTCONFIG=True
 
@@ -28,11 +29,8 @@ mpl.font_manager.USE_FONTCONFIG=True
 
 df = pd.read_csv("runtime-analysis.csv", skipinitialspace=True)
 
-df["hooks"] = df["instrumented hooks"]
-print df
-
 # drop some columns and rows that add clutter
-df = df.drop(["comment", "analysis"], axis=1)
+df = df.drop(["comment", "analysis", "browser"], axis=1)
 
 # add Program column that combines all polybench programs into one and gives more readable names
 df.loc[df.program == "pspdfkit", "program_group"] = "PSPDFKit"
@@ -41,7 +39,7 @@ df.loc[(df.program != "pspdfkit") & (df.program != "UE4Game-HTML5-Shipping"), "p
 
 # add overhead column that is relative to the none execution
 for i, row in df.iterrows():
-	none_seconds = df[(df.hooks == "none") & (df.program == row.program)].iloc[0].seconds
+	none_seconds = df[(df.hooks == "none") & (df.program == row.program)].seconds.median()
 	df.ix[i, "overhead"] = row.seconds / float(none_seconds)
 
 # df.replace("br_table","   br_table",inplace=True)
@@ -53,32 +51,32 @@ for i, row in df.iterrows():
 # print df_
 
 # custom hooks sort order
-# df.hooks = pd.Categorical(df.hooks, [
-# 	# "start",
-# 	# "none",
-# 	# "nop",
-# 	# "unreachable",	
-# 	# "memory_size",
-# 	# "memory_grow",
-# 	# "select",
-# 	"drop",
-# 	"load",
-# 	"store",
-# 	"call",
-# 	"return",
-# 	"const",
-# 	"unary",
-# 	"binary",
-# 	"global",
-# 	"local",
-# 	"begin",
-# 	"end",
-# 	"if",
-# 	"br",
-# 	"br_if",
-# 	"br_table",
-# 	# "all",
-# ])
+df.hooks = pd.Categorical(df.hooks, [
+	"none",
+	"start",
+	"nop",
+	"unreachable",	
+	"memory_size",
+	"memory_grow",
+	"select",
+	"drop",
+	"load",
+	"store",
+	"call",
+	"return",
+	"const",
+	"unary",
+	"binary",
+	"global",
+	"local",
+	"begin",
+	"end",
+	"if",
+	"br",
+	"br_if",
+	"br_table",
+	"all",
+])
 
 # print df
 # print df.dtypes
@@ -93,15 +91,18 @@ fp = mpl.font_manager.FontProperties(fname="/usr/share/fonts/truetype/fira/FiraS
 # print fp
 print mpl.font_manager.findfont("Fira Sans")
 
-g = sns.factorplot(x="hooks", y="overhead", hue="program", kind="bar", aspect=4, size=4, data=df, legend=False, estimator=np.median,errwidth=.5,capsize=.1)
+g = sns.factorplot(x="hooks", y="overhead", hue="program_group", kind="bar", aspect=4, size=4, data=df, legend=False, estimator=sp.stats.gmean,
+	errwidth=.5,
+	# capsize=.1,
+)
 g.ax.yaxis.set_minor_locator(mpl.ticker.AutoMinorLocator())
 g.despine(offset=4,bottom=True)
 plt.xlabel("Instrumented Hooks", fontproperties=fp, fontsize=11)
 plt.ylabel("Relative Runtime", fontproperties=fp, fontsize=11,
 #	position=(0,.3)
 )
-legend = plt.legend(loc="upper right",bbox_to_anchor=(1.04, 1.07),frameon=1,framealpha=1,prop=fp, fontsize=11)
-legend.get_frame().set_linewidth(0)
+# legend = plt.legend(loc="upper right",bbox_to_anchor=(1.04, 1.07),frameon=1,framealpha=1,prop=fp, fontsize=11)
+# legend.get_frame().set_linewidth(0)
 plt.tick_params(axis='x', 
 	bottom=False, # ticks along the bottom edge are off
 	labelbottom=True)
