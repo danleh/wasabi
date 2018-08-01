06e3d405ba24b354850f265d1cd484b5e9c8b814
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-08-01 16:34:17 +0200

    update code size figure: geomean, no error bars, show more hooks, 2 column wide

--- eval/results/code-size-figure.py
+++ eval/results/code-size-figure.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
+import scipy as sp
 
 mpl.font_manager.USE_FONTCONFIG=True
 
@@ -31,35 +32,32 @@ df = pd.read_csv("code-size.csv", skipinitialspace=True)
 # drop some columns and rows that add clutter
 df = df.drop(["path"], axis=1)
 
-# add Program column that combines all polybench programs into one and gives more readable names
-df.loc[df.filename == "pspdfkit", "program"] = "PSPDFKit"
-df.loc[df.filename == "UE4Game-HTML5-Shipping", "program"] = "Unreal Engine 4"
-df.loc[(df.filename != "pspdfkit") & (df.filename != "UE4Game-HTML5-Shipping"), "program"] = "PolyBench (median)"
+# add program_group column that combines all polybench programs into one and gives more readable names
+df.loc[df.program == "pspdfkit", "program_group"] = "PSPDFKit"
+df.loc[df.program == "UE4Game-HTML5-Shipping", "program_group"] = "Unreal Engine 4"
+df.loc[(df.program != "pspdfkit") & (df.program != "UE4Game-HTML5-Shipping"), "program_group"] = "PolyBench (geomean)"
 
 # add overhead column that is relative to the original size
 for i, row in df.iterrows():
-	original_bytes = df[(df.hooks == "original") & (df.filename == row.filename)].iloc[0].bytes
-	df.ix[i, "overhead"] = row.bytes / float(original_bytes)
-
-# remove the original, now no longer needed
-df = df[df.hooks != "original"]
+	original_bytes = df[(df.hooks == "original") & (df.program == row.program)].bytes
+	assert len(original_bytes) == 1
+	df.ix[i, "overhead"] = row.bytes / float(original_bytes.iloc[0])
 
 # df.replace("br_table","   br_table",inplace=True)
-
 # print df[df.hooks == "all"].groupby("program").median()
 
-df_ = df.groupby(["hooks", "program"]).median()
+df_ = df.groupby(["hooks", "program_group"]).median()
 print df_[df_.overhead > 1.3]
 
 # custom hooks sort order
 df.hooks = pd.Categorical(df.hooks, [
 	# "start",
 	# "none",
-	# "nop",
-	# "unreachable",	
-	# "memory_size",
-	# "memory_grow",
-	# "select",
+	"nop",
+	"unreachable",	
+	"memory_size",
+	"memory_grow",
+	"select",
 	"drop",
 	"load",
 	"store",
@@ -92,24 +90,40 @@ fp = mpl.font_manager.FontProperties(fname="/usr/share/fonts/truetype/fira/FiraS
 # print fp
 print mpl.font_manager.findfont("Fira Sans")
 
-g = sns.factorplot(x="hooks", y="overhead", hue="program", kind="bar", aspect=2.2, size=2.6, data=df, legend=False, estimator=np.median,errwidth=.5,capsize=.1)
+g = sns.factorplot(x="hooks", y="overhead", hue="program_group", kind="bar", aspect=5, size=2.2, data=df, legend=False,
+	estimator=sp.stats.gmean,	
+	errwidth=0,
+	# capsize=.1
+)
 g.ax.yaxis.set_minor_locator(mpl.ticker.AutoMinorLocator())
 g.despine(offset=4,bottom=True)
-plt.xlabel("Instrumented Hooks", fontproperties=fp, fontsize=11)
+plt.xlabel("Instrumented Hooks", fontproperties=fp, fontsize=11,
+	labelpad=-12
+)
 plt.ylabel("Relative Binary Size", fontproperties=fp, fontsize=11,
 #	position=(0,.3)
 )
-legend = plt.legend(loc="upper right",bbox_to_anchor=(1.04, 1.07),frameon=1,framealpha=1,prop=fp, fontsize=11)
+legend = plt.legend(
+	loc="upper right",
+	bbox_to_anchor=(1.02, 1.1),
+	frameon=1,
+	framealpha=1,
+	prop=fp,
+	fontsize=11
+)
 legend.get_frame().set_linewidth(0)
 plt.tick_params(axis='x', 
 	bottom=False, # ticks along the bottom edge are off
 	labelbottom=True)
+plt.ylim((0,3))
 g.ax.yaxis.grid(b=True,which="major",color=".7",linewidth=.5)
 g.ax.yaxis.grid(b=True,which="minor",color=".85",linewidth=.5,linestyle="--")
 # g.set_xticklabels(rotation=45, ha="center", position=(0,-.02), fontproperties="Inconsolata",fontsize="11", rotation_mode="anchor")
 g.set_xticklabels(fontproperties="Inconsolata",fontsize="10",
 	rotation=45,
-	position=(0,0.05)
+	rotation_mode="anchor",
+	ha="right",
+	position=(0,0.06),
 )
 g.set_yticklabels(g.ax.get_yticks(), fontproperties=fp)
 
@@ -124,6 +138,6 @@ def change_width(ax, new_value) :
         # we recenter the bar
         patch.set_x(patch.get_x() + diff * .5)
 
-change_width(g.ax, .25)
+change_width(g.ax, .24)
 
 g.savefig("code-size.pdf")
