cb81d790fc131ff2585d46d66727361e6858cfe5
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-08-03 11:14:59 +0200

    use our own emrun to benchmark UE4, use CPU intensive mode (i.e., no audio, webgl)

--- eval/6-run-ue4.sh
+++ eval/6-run-ue4.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+
+firefox_bin="$(readlink -f browsers/firefox-63-nightly/firefox)"
+firefox_args="-no-remote -profile $(readlink -f browsers/firefox-profile)"
+
+chrome_bin="$(readlink -f browsers/chromium-69-nightly/chrome)"
+chrome_args="--user-data-dir=$(readlink -f browsers/chromium-profile)"
+
+emrun_output="$(readlink -f results/runtime-analysis.csv)"
+
+analysis=$1
+hooks=$2
+comment="$3"
+
+if [ -z $analysis ] ||  [ -z $hooks ]
+then
+	echo "usage: $0 <analysis> <hooks> [<comment>]"
+	echo "(all arguments are just passed as strings into the results.csv)"
+	exit
+fi
+
+# ensure emcc is on PATH, e.g., by running 
+# FIXME: the following path isn't machine-independent
+source ~/Documents/SOLA/WebAssembly/tools/emsdk/emsdk_env.sh
+
+echo -n "firefox, $analysis, $hooks, \"$comment\", UE4Game-HTML5-Shipping, " >> $emrun_output
 cd programs-analysis/EpicZenGarden/
-sh start_server
-cd -
+emrun --no_emrun_detect --log_stdout "$emrun_output" --browser "$firefox_bin" --browser_args "$firefox_args" --serve_root . "2017-03-16-ZenGarden/EpicZenGarden.html?playback&novsync&noaudio&fakegl"
+cd - > /dev/null
