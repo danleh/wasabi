ecf6f520d066840d985d14cdd589512ac9d3a3c1
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-08-03 23:05:48 +0200

    improve PSPDF splicing: warn if insertion point could not be found (happened to me after rebuilding PSPDFKit)

--- eval/pspdfkit-splice.py
+++ eval/pspdfkit-splice.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
 import sys
-to_insert = sys.stdin.read().replace("'", "").replace("\n", "\\n")
-pspdfkit = open(sys.argv[1]).read()
-modified = pspdfkit.replace("return n(476)('", "return n(476)('" + to_insert)
-print modified
 
+# this is just before the BLOB of JavaScript that the workers get initially (if it changes, you might find it by searching for the header comment, e.g., "* PSPDFKit for Web")
+# we will prepend our own JS before theirs (but after this haystack)
+HAYSTACK = "return n(382)('"
+
+# remove single quotes, escape newlines
+# TODO warn if stdin contains single quotes, code might break!
+to_insert = sys.stdin.read().replace("'", "").replace("\n", "\\n")
+
+# pspdfkit.js library file from command line argument
+pspdfkit = open(sys.argv[1]).read()
+
+# insert our own code just before the worker JavaScript
+if HAYSTACK not in pspdfkit:
+	print >> sys.stderr, "could not find beginning of Worker JavaScript code, did not insert own code into PSPDFKit library, potentially broken!"
+	exit(1)
+
+modified = pspdfkit.replace(HAYSTACK, HAYSTACK + to_insert)
+print modified
