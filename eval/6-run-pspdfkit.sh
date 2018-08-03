695534609e7781cb53ed54fc595b35776c9bb30f
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-08-03 22:40:23 +0200

    adapt pspdfkit benchmark runner from UE4

--- eval/6-run-pspdfkit.sh
+++ eval/6-run-pspdfkit.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
-cd programs-analysis/pspdfkit-webassembly-benchmark-master/
-npm start
-cd -
+
+firefox_bin="$(readlink -f browsers/firefox-63-nightly/firefox)"
+firefox_args="-no-remote -profile $(readlink -f browsers/firefox-profile)"
+
+chrome_bin="$(readlink -f browsers/chromium-69-nightly/chrome)"
+chrome_args="--user-data-dir=$(readlink -f browsers/chromium-profile)"
+
+emrun_output="$(readlink -f results/runtime-analysis.csv)"
+
+analysis=$1
+hooks=$2
+comment="$3"
+
+if [ -z $analysis ] ||  [ -z $hooks ]
+then
+	echo "usage: $0 <analysis> <hooks> [<comment>]"
+	echo "(all arguments are just passed as strings into the results.csv)"
+	exit
+fi
+
+# ensure emcc is on PATH, e.g., by running 
+# FIXME: the following path isn't machine-independent
+source ~/Documents/SOLA/WebAssembly/tools/emsdk/emsdk_env.sh
+
+echo -n "firefox, $analysis, $hooks, \"$comment\", pspdfkit, " >> $emrun_output
+cd programs-analysis/pspdfkit/
+# save some time: don't do 20 runs (inside the benchmark => runsScaleFactor), because we average over multiple ("our") runs anyway
+emrun --no_emrun_detect --log_stdout "$emrun_output" --browser "$firefox_bin" --browser_args "$firefox_args" index.html?runsScaleFactor=0.5
+cd - > /dev/null
+
+sleep 2s
