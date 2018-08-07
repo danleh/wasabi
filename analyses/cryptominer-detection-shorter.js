3d683ffc5b7c272b79c23eb2f59313a0398e24b2
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-08-07 17:09:56 +0200

    shorten cryptominer detection code to version of paper

--- analyses/cryptominer-detection-shorter.js
+++ analyses/cryptominer-detection-shorter.js
@@ -1,16 +1,10 @@
-{
-	const signature = {};
-
-	Wasabi.analysis.binary = function(loc, op) {
-		switch (op) {
-			case "i32.add":
-			case "i32.and":
-			case "i32.shl":
-			case "i32.shr_u":
-			case "i32.xor":
-				signature[op] = (signature[op] || 0) + 1;
-		}
-	};
-
-	Wasabi.analysisResult = signature;
-}
+const signature = {};
+Wasabi.analysis.binary = function(loc, op) {
+	switch (op) {
+		case "i32.add":
+		case "i32.and":
+		case "i32.shl":
+		case "i32.shr_u":
+		case "i32.xor":
+			signature[op] = (signature[op] || 0) + 1;
+}};
