25d964aedfb3aab78ee2eac8bbd4b91565115974
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-08-07 17:09:02 +0200

    update figure and give more output in Pyhton

--- eval/results/code-size.py
+++ eval/results/code-size.py
@@ -35,11 +35,15 @@ df = df.drop(["path"], axis=1)
 # add program_group column that combines all polybench programs into one and gives more readable names
 df.loc[df.program == "pspdfkit", "program_group"] = "PSPDFKit"
 df.loc[df.program == "UE4Game-HTML5-Shipping", "program_group"] = "Unreal Engine 4"
-df.loc[(df.program != "pspdfkit") & (df.program != "UE4Game-HTML5-Shipping"), "program_group"] = "PolyBench (geomean)"
+df.loc[(df.program != "pspdfkit") & (df.program != "UE4Game-HTML5-Shipping"), "program_group"] = "PolyBench (mean)"
+
+print df
+df = df.groupby(["program_group", "hooks"]).bytes.mean().reset_index()
+print df
 
 # add overhead column that is relative to the original size
 for i, row in df.iterrows():
-	original_bytes = df[(df.hooks == "original") & (df.program == row.program)].bytes
+	original_bytes = df[(df.hooks == "original") & (df.program_group == row.program_group)].bytes
 	assert len(original_bytes) == 1
 	original_bytes = original_bytes.iloc[0]
 	df.ix[i, "overhead"] = (row.bytes - original_bytes) / (original_bytes / 100.0)
@@ -47,12 +51,15 @@ for i, row in df.iterrows():
 # df.replace("br_table","   br_table",inplace=True)
 # print df[df.hooks == "all"].groupby("program").median()
 
-print df.groupby(["hooks"]).overhead.describe()
+print df.groupby(["hooks"]).overhead.min()
+print df.groupby(["hooks"]).overhead.max()
+print df.groupby(["hooks", "program_group"]).overhead.mean().reset_index()
 # df_ = df.groupby(["hooks", "program"]).median().reset_index()
 # print df_[(df_.overhead < 1.1) & (df_.overhead > 1.01)]
 # print df_[(df_.hooks == "all")]
 
 # custom hooks sort order
+df.program_group = pd.Categorical(df.program_group, ["PolyBench (mean)", "PSPDFKit", "Unreal Engine 4"])
 df.hooks = pd.Categorical(df.hooks, [
 	# "start",
 	# "none",
@@ -94,8 +101,8 @@ fp = mpl.font_manager.FontProperties(fname="/usr/share/fonts/truetype/fira/FiraS
 print mpl.font_manager.findfont("Fira Sans")
 
 g = sns.factorplot(x="hooks", y="overhead", hue="program_group", kind="bar", aspect=5, size=2.2, data=df, legend=False,
-	estimator=sp.stats.gmean,	
-	errwidth=0,
+	# estimator=sp.stats.gmean,	
+	# errwidth=0,
 	# capsize=.1
 )
 g.ax.yaxis.set_minor_locator(mpl.ticker.AutoMinorLocator())
@@ -118,10 +125,10 @@ legend.get_frame().set_linewidth(0)
 plt.tick_params(axis='x', 
 	bottom=False, # ticks along the bottom edge are off
 	labelbottom=True)
-plt.ylim((-20,200))
+# plt.ylim((-20,200))
 g.ax.yaxis.grid(b=True,which="major",color=".7",linewidth=.5)
 g.ax.yaxis.grid(b=True,which="minor",color=".85",linewidth=.5,linestyle="--")
-plt.axhline(y=0,linewidth=.7,color="black")
+# plt.axhline(y=0,linewidth=.7,color="black")
 # g.set_xticklabels(rotation=45, ha="center", position=(0,-.02), fontproperties="Inconsolata",fontsize="11", rotation_mode="anchor")
 g.set_xticklabels(fontproperties="Inconsolata",fontsize="10",
 	rotation=45,
