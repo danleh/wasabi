6eac82c47b3e7f0a36dbae059a955108f07cde18
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2018-12-29 19:11:24 +0100

    extract constants

--- lib/leb128/src/lib.rs
+++ lib/leb128/src/lib.rs
@@ -16,8 +16,18 @@ pub trait WriteLeb128<T>: io::Write {
     fn write_leb128(&mut self, value: T) -> io::Result<usize>;
 }
 
-fn sign_bit(byte: u8) -> bool {
-    byte & 0x40 == 0x40
+/* Trait implementations */
+
+const CONTINUATION_BIT: u8 = 0x80;
+
+fn is_set_continuation_bit(byte: u8) -> bool {
+    byte & CONTINUATION_BIT == CONTINUATION_BIT
+}
+
+const SIGN_BIT: u8 = 0x40;
+
+fn is_set_sign_bit(byte: u8) -> bool {
+    byte & SIGN_BIT == SIGN_BIT
 }
 
 macro_rules! signed {
@@ -34,12 +44,12 @@ macro_rules! impl_leb128_integer {
                 // useful if you want to preserve length when encoding this LEB128 again (unused currently though)
                 let mut _bytes_read = 0;
                 let mut shift = 0;
-                let mut byte = 0x80;
+                let mut byte = CONTINUATION_BIT;
 
-                while byte & 0x80 != 0 {
+                while is_set_continuation_bit(byte) {
                     byte = self.read_u8()?;
                     // mask off continuation bit from byte and prepend lower 7 bits to value
-                    if let Some(high_bits) = ((byte & 0x7f) as $T).checked_shl(shift) {
+                    if let Some(high_bits) = ((byte & !CONTINUATION_BIT) as $T).checked_shl(shift) {
                         value |= high_bits;
                     } else {
                         return Err(io::Error::new(io::ErrorKind::InvalidData, format!("LEB128 to {} overflow", stringify!($T))));
@@ -50,7 +60,7 @@ macro_rules! impl_leb128_integer {
 
                 if signed!($T) {
                     let (sign_extend, did_overflow) = (!0 as $T).overflowing_shl(shift);
-                    if sign_bit(byte) && !did_overflow {
+                    if is_set_sign_bit(byte) && !did_overflow {
                         value |= sign_extend;
                     }
                 }
@@ -66,14 +76,14 @@ macro_rules! impl_leb128_integer {
 
                 while more_bytes {
                     // select low 7 bits of value
-                    let mut byte_to_write = value as u8 & 0x7F;
+                    let mut byte_to_write = value as u8 & !CONTINUATION_BIT;
                     // sign extends, important for signed integers!
                     value >>= 7;
                     bytes_written += 1;
 
-                    more_bytes = value != if signed!($T) && sign_bit(byte_to_write) { !0 } else { 0 };
+                    more_bytes = value != if signed!($T) && is_set_sign_bit(byte_to_write) { !0 } else { 0 };
                     if more_bytes {
-                        byte_to_write |= 0x80;
+                        byte_to_write |= CONTINUATION_BIT;
                     }
                     self.write_u8(byte_to_write)?;
                 }
