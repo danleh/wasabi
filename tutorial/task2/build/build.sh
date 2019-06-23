3a715f4583f89b321213c1e0756f103327a2b777
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2019-06-23 03:16:26 +0200

    remove build dir for task2, only confusing

--- tutorial/task2/build/build.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/kripken/BananaBread.git
-pushd BananaBread/cube2/src/web
-emmake make
-cd ../..
-zip -9 -r ../../bb.zip assets base.data bb.html bb.js bb.wasm character.data css game js low.data
-popd
