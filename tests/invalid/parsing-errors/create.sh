6974e7ed722e8ca5ecb1dfac7608c029426a3865
Daniel Lehmann <mail@dlehmann.eu>
2020-03-14 17:23:21 +0100

    add more tests for parsing error handling (vs. wabt wasm-validate)

--- tests/invalid/parsing-errors/create.sh
+++ tests/invalid/parsing-errors/create.sh
@@ -25,6 +25,48 @@ echo -ne '\x60\x00\x00\x60\x00\x00' >> type-section-too-many-elements-for-size.w
 # type count: 1
 echo -ne '\x00asm\x01\x00\x00\x00\x01\x02\x01' > type-section-elements-missing.wasm
 
+echo -ne '\x00asm\x01\x00\x00\x00' > only-code-no-function-section.wasm
+# section id: 0xa = 10 == code section
+# size of whole section in bytes: 11
+# number of code entries: 3
+echo -ne '\x0a\x0b\x03' >> only-code-no-function-section.wasm
+# code entry 1
+# size in bytes: 2
+# number of locals: 0
+# expression: just end (0xb)
+echo -ne '\x02\x00\x0b' >> only-code-no-function-section.wasm
+# code entry 2
+# size in bytes: 3
+# number of locals: 0
+# expression: nop (0x1); end (0xb)
+echo -ne '\x03\x00\x01\x0b' >> only-code-no-function-section.wasm
+# code entry 3
+# size in bytes: 2
+# number of locals: 0
+# expression: just end (0xb)
+echo -ne '\x02\x00\x0b' >> only-code-no-function-section.wasm
+
+echo -ne '\x00asm\x01\x00\x00\x00' > code-element-size-too-small.wasm
+# section id: 0xa = 10 == code section
+# size of whole section in bytes: 5 (correct)
+# number of code entries: 1
+echo -ne '\x0a\x05\x01' >> code-element-size-too-small.wasm
+# code entry 1
+# size in bytes: 2 (one too short!)
+# number of locals: 0
+# expression: nop (0x1); end (0xb)
+echo -ne '\x02\x00\x01\x0b' >> code-element-size-too-small.wasm
+
+echo -ne '\x00asm\x01\x00\x00\x00' > code-element-size-too-large.wasm
+# section id: 0xa = 10 == code section
+# size of whole section in bytes: 5 (correct)
+# number of code entries: 1
+echo -ne '\x0a\x05\x01' >> code-element-size-too-large.wasm
+# code entry 1
+# size in bytes: 4 (one too long!)
+# number of locals: 0
+# expression: nop (0x1); end (0xb)
+echo -ne '\x04\x00\x01\x0b' >> code-element-size-too-large.wasm
 
 # check against official wasm-validate from WABT to make sure all of these are really invalid
 # start with smallest first
@@ -34,4 +76,5 @@ do
     echo "$file"
     wasm-validate "$file"
     cargo run -q -- "$file"
+    # wasabi "$file"
 done
