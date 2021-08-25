59116a16b2c04194ec6c0e06c969a426aa12597a
Daniel Lehmann <mail@dlehmann.eu>
2021-08-25 12:07:57 +0200

    fix renamed functions in tests

--- lib/wasm/src/tests.rs
+++ lib/wasm/src/tests.rs
@@ -130,8 +130,8 @@ fn section_offsets_like_objdump() {
             lowlevel::NameSection { subsections: Vec::new() }
         ))), vec![0x1f]);
     // Also try the (only) function code offset, for completion.
-    assert_eq!(offsets.function_idx_to_code(Idx::from(0)), Some(0x17));
-    assert_eq!(offsets.function_code_to_idx(0x17), Some(Idx::from(0)));
+    assert_eq!(offsets.function_idx_to_offset(Idx::from(0)), Some(0x17));
+    assert_eq!(offsets.function_offset_to_idx(0x17), Some(Idx::from(0)));
 }
 
 #[test]
@@ -140,15 +140,15 @@ fn code_offsets_like_objdump() {
 
     // Test first two and last two functions.
     // Expected values are taken from wasm-objdump output.
-    assert_eq!(offsets.function_idx_to_code(Idx::from(383)), Some(0x5522));
-    assert_eq!(offsets.function_code_to_idx(0x5522), Some(Idx::from(383)));
-    assert_eq!(offsets.function_idx_to_code(Idx::from(384)), Some(0x5545));
-    assert_eq!(offsets.function_code_to_idx(0x5545), Some(Idx::from(384)));
+    assert_eq!(offsets.function_idx_to_offset(Idx::from(383)), Some(0x5522));
+    assert_eq!(offsets.function_offset_to_idx(0x5522), Some(Idx::from(383)));
+    assert_eq!(offsets.function_idx_to_offset(Idx::from(384)), Some(0x5545));
+    assert_eq!(offsets.function_offset_to_idx(0x5545), Some(Idx::from(384)));
 
-    assert_eq!(offsets.function_idx_to_code(Idx::from(3641)), Some(0x1e38b7));
-    assert_eq!(offsets.function_code_to_idx(0x1e38b7), Some(Idx::from(3641)));
-    assert_eq!(offsets.function_idx_to_code(Idx::from(3642)), Some(0x1e38d2));
-    assert_eq!(offsets.function_code_to_idx(0x1e38d2), Some(Idx::from(3642)));
+    assert_eq!(offsets.function_idx_to_offset(Idx::from(3641)), Some(0x1e38b7));
+    assert_eq!(offsets.function_offset_to_idx(0x1e38b7), Some(Idx::from(3641)));
+    assert_eq!(offsets.function_idx_to_offset(Idx::from(3642)), Some(0x1e38d2));
+    assert_eq!(offsets.function_offset_to_idx(0x1e38d2), Some(Idx::from(3642)));
 }
 
 #[test]
