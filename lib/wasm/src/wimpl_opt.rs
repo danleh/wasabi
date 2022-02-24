6872117e962e769bef5d7b31ced0952a25df5ce7
michelledaviest <michelledaviest@gmail.com>
2022-02-24 17:54:17 -0500

    added file to do optimizations on wimpl module

---
+++ lib/wasm/src/wimpl_opt.rs
@@ -0,0 +1,18 @@
+/* 
+use crate::{wimpl::{Func, self, Expr::Call, Function, Var}, FunctionType};
+
+fn constant_propogation (module: wimpl::Module) -> wimpl::Module {
+
+}
+
+pub fn wimpl_optimize (path: impl AsRef<Path>) -> Result<Module, String> {
+    let module = wimplify_module(&highlevel::Module::from_file(path.as_ref()).expect("path should point to a valid wasm file")); 
+    
+}
+
+#[test]
+fn test() {
+    println!("hi"); 
+}
+
+*/
