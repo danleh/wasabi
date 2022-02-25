0de5f57dfbac44c197adb64992a15b27260699a4
michelledaviest <michelledaviest@gmail.com>
2022-02-24 19:53:27 -0500

    WIP constant propagation: propagation working, need to cleanup unused stack slots and code

--- lib/wasm/src/wimpl.rs
+++ lib/wasm/src/wimpl.rs
@@ -2,7 +2,7 @@ use std::{
     fmt::{self, Write},
     io::{self, ErrorKind},
     path::Path,
-    str::FromStr,
+    str::FromStr, collections::HashMap,
 };
 
 use crate::{highlevel::{MemoryOp, Global, Table}, types::{InferredInstructionType, TypeChecker}, Val, ValType, Idx, BlockType};
@@ -2125,6 +2125,222 @@ fn test(path_wimpl: &str, path_wasm: &str) {
     let expected = Body(Stmt::from_text_file(path_wimpl).unwrap());
     assert_eq!(actual, expected, "\nACTUAL: {}\nEXPECTED: {}\n", actual, expected);
 }
+
+#[derive(Debug)]
+enum Value {
+    Top, 
+    Var(Var)
+}
+
+fn populate_var_map<'module>(
+    stmts: &mut impl Iterator<Item=&'module Stmt>, 
+    map: &mut HashMap<Var, Value>, 
+) -> Vec<Stmt> {
+    let mut res: Vec<Stmt> = Vec::new(); 
+
+    while let Some(stmt) = stmts.next() { 
+        
+        match stmt {
+            
+            Stmt::Assign { lhs, type_, rhs } => {
+                
+                let stmt_type = *type_; 
+
+                let mut check = |lhs: Var| -> Option<Var> { //why on earth should it be mutable 
+                    if map.contains_key(&lhs) {
+                        match *map.get_mut(&lhs).expect("lhs should exist in var map") {
+                            Value::Top => None,
+                            Value::Var(rhs) => Some(rhs)
+                        }
+                    } else {
+                        None   
+                    }
+                }; 
+
+                let mut check_vec = |lhs_vec: Vec<Var>| -> Vec<Var> {
+                    let mut res_vec = Vec::new();
+                    for lhs in lhs_vec {
+                        if let Some(replacement_lhs) = check(lhs) {
+                            res_vec.push(replacement_lhs)
+                        } else {
+                            res_vec.push(lhs); 
+                        }
+                    }; 
+                    res_vec
+                }; 
+
+                match rhs {
+                    
+                    Expr::VarRef(var) => {
+                        
+                        if map.contains_key(lhs) {
+                            *map.get_mut(lhs).expect("lhs should exist in var map") = Value::Top; 
+                        } else {
+                            map.insert(*lhs, Value::Var(*var));  
+                        }
+                        /* 
+                        if let Some(replacement_var) = check(*var) {
+                            res.push(Stmt::Assign{
+                                lhs: *lhs,
+                                type_: stmt_type,
+                                rhs: Expr::VarRef(replacement_var),                                
+                            })
+                        } else {
+                            res.push(stmt.clone()); 
+                        }
+                        */
+                        res.push(stmt.clone()); 
+                    },
+
+                    Expr::Const(_var_) => res.push(stmt.clone()), 
+
+                    Expr::Load { op, memarg, addr } => {
+                        if let Some(replacement_var) = check(*addr) {
+                            res.push(Stmt::Assign{
+                                lhs: *lhs,
+                                type_: stmt_type,
+                                rhs: Expr::Load{
+                                    op: *op,
+                                    memarg: *memarg,
+                                    addr: replacement_var,
+                                },
+                            })
+                        } else {
+                            res.push(stmt.clone()); 
+                        } 
+                    },
+
+                    Expr::MemoryGrow { pages } => {
+                        if let Some(replacement_var) = check(*pages) {
+                            res.push(Stmt::Assign{
+                                lhs: *lhs,
+                                type_: stmt_type,
+                                rhs: Expr::MemoryGrow {
+                                    pages: replacement_var,
+                                },
+                            })
+                        } else {
+                            res.push(stmt.clone()); 
+                        } 
+                    },
+                    
+                    Expr::Numeric { op, args } => {
+                        res.push(Stmt::Assign{
+                            lhs: *lhs,
+                            type_: stmt_type,
+                            rhs: Expr::Numeric{
+                                op: *op, 
+                                args: check_vec(args.clone()) 
+                            },
+                        })                        
+                    } 
+
+                    Expr::Call { func, args } => {
+                        res.push(Stmt::Assign{
+                            lhs: *lhs,
+                            type_: stmt_type, 
+                            rhs: Expr::Call{
+                                func: func.clone(), 
+                                args: check_vec(args.clone()) 
+                            },
+                        })
+                    }
+
+                    Expr::CallIndirect { type_, table_idx, args } => {
+                        res.push(Stmt::Assign{
+                            lhs: *lhs,
+                            type_: stmt_type,
+                            rhs: Expr::CallIndirect{
+                                args: check_vec(args.clone()),
+                                type_: type_.clone(),
+                                table_idx: *table_idx, 
+                            },
+                        })
+                    },
+                
+                    _ => res.push(stmt.clone())
+                }
+            },
+            
+            Stmt::Block { body, end_label: _ } | 
+            Stmt::Loop { begin_label: _, body } => {
+                let mut body_stmts = body.0.iter();
+                let body_stmts = populate_var_map(&mut body_stmts, map);
+                res.push(match stmt {
+                    
+                    Stmt::Block { body: _, end_label } => {
+                        Stmt::Block{
+                            body: Body(body_stmts),
+                            end_label: *end_label,
+                        }
+                    },
+
+                    Stmt::Loop { begin_label, body: _ } => {
+                        Stmt::Loop{
+                            begin_label: *begin_label,
+                            body: Body(body_stmts),
+                        }
+                    },
+                    
+                    _ => panic!("no statements other than block and loop expected")  
+                })
+            }, 
+
+            Stmt::If { condition , if_body, else_body } => {
+                let mut if_body_stmts = if_body.0.iter();
+                let if_body = Body(populate_var_map(&mut if_body_stmts, map));
+
+                let else_body = if let Some(else_body) = else_body {
+                    let mut else_body_stmts = else_body.0.iter();
+                    Some(Body(populate_var_map(&mut else_body_stmts, map)))    
+                } else {
+                    None
+                }; 
+
+                res.push(Stmt::If{
+                    condition: *condition,
+                    if_body,
+                    else_body,
+                })
+            },
+
+            _ => res.push(stmt.clone()) 
+        }
+    }; 
+    res
+}
+
+fn constant_propogation (module: &mut Module) -> Module {
+    let mut opt_funcs = Vec::new();  
+    for func in module.functions.clone() { //TODO dont want to clone and yet 
+        let mut stmts = func.body.0.iter();
+        let mut map = HashMap::new(); 
+        let opt_stmts = populate_var_map(&mut stmts, &mut map); 
+        for x in map {
+            println!("{:?}", x); 
+        }        
+        opt_funcs.push(Function{
+            name: func.name,
+            type_: func.type_,
+            body: Body(opt_stmts),
+            export: func.export,
+        })
+    }; 
+    Module { functions: opt_funcs, globals: module.globals.clone(), tables: module.tables.clone() }
+}
+
+pub fn wimpl_optimize (path: impl AsRef<Path>) -> Result<Module, String> {
+    let mut module = wimplify_module(&highlevel::Module::from_file(path.as_ref()).expect("path should point to a valid wasm file"))?; 
+    let module = constant_propogation(&mut module);   
+    Ok(module)   
+}
+
+#[test]
+fn opt_test() {
+    let module = wimpl_optimize("tests/wimpl/local/local.wasm");
+    println!("{}", module.unwrap()); 
+}
+
 
 #[test]
 fn constant() {
--- lib/wasm/src/wimpl_opt.rs
+++ lib/wasm/src/wimpl_opt.rs
@@ -1,5 +1,4 @@
-/* 
-use crate::{wimpl::{Func, self, Expr::Call, Function, Var}, FunctionType};
+//use crate::{wimpl::{Func, self, Expr::Call, Function, Var}, FunctionType};
 
 fn constant_propogation (module: wimpl::Module) -> wimpl::Module {
 
@@ -7,7 +6,7 @@ fn constant_propogation (module: wimpl::Module) -> wimpl::Module {
 
 pub fn wimpl_optimize (path: impl AsRef<Path>) -> Result<Module, String> {
     let module = wimplify_module(&highlevel::Module::from_file(path.as_ref()).expect("path should point to a valid wasm file")); 
-    
+    let module = constant_propagation(module);     
 }
 
 #[test]
@@ -15,4 +14,3 @@ fn test() {
     println!("hi"); 
 }
 
-*/
