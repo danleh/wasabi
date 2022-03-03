14691fa7dbfacbb3a5456499fc10a3dc1ee2a601
Daniel Lehmann <mail@dlehmann.eu>
2022-03-03 21:16:16 +0100

    extract abstracting of call_indirect exprs into its own utility function

--- lib/wasm/src/wimpl/analyze.rs
+++ lib/wasm/src/wimpl/analyze.rs
@@ -1,9 +1,9 @@
-use std::{fmt, iter::FromIterator, cmp::Reverse};
+use std::{cmp::Reverse, fmt, iter::FromIterator};
 
 use regex::Regex;
 use rustc_hash::FxHashMap;
 
-use crate::wimpl::{Stmt, Expr, Var, Body, Module};
+use crate::wimpl::{Body, Expr, Module, Stmt, Var};
 
 #[derive(Debug, Default, Clone, Eq, PartialEq)]
 pub struct VarExprMap(FxHashMap<Var, Option<Expr>>);
@@ -12,24 +12,25 @@ pub struct VarExprMap(FxHashMap<Var, Option<Expr>>);
 pub enum VarExprMapResult<'a> {
     Precise(&'a Expr),
     Top,
-    Uninitialized(Var)
+    Uninitialized(Var),
 }
 
 impl VarExprMap {
     pub fn from_body(body: &Body) -> Self {
         let mut map = Self::default();
 
-        body.visit_stmt_pre_order(|stmt| 
+        body.visit_stmt_pre_order(|stmt| {
             if let Stmt::Assign { lhs, type_: _, rhs } = stmt {
                 map.add(lhs, rhs)
             }
-        );
+        });
 
         map
     }
 
     pub fn add(&mut self, var: &Var, expr: &Expr) {
-        self.0.entry(*var)
+        self.0
+            .entry(*var)
             // Overapproximate if there was already a prior assignment to that variable.
             .and_modify(|old_expr| *old_expr = None)
             .or_insert_with(|| Some(expr.clone()));
@@ -67,29 +68,34 @@ pub fn collect_call_indirect_idx_expr(module: &Module) -> Vec<(String, usize)> {
     let mut result: FxHashMap<String, usize> = FxHashMap::default();
     for func in &module.functions {
         use Expr::*;
-        func.body.visit_expr_pre_order(|expr| 
+        func.body.visit_expr_pre_order(|expr| {
             if let CallIndirect { type_: _, table_idx, args: _ } = expr {
-                let table_idx = table_idx.to_string();
-
-                // HACK Remove some stuff that is irrelevant for our analysis
-                lazy_static::lazy_static! {
-                    static ref MEMARG: Regex = Regex::new(r"\s+offset=\d+\s+").unwrap();
-                    static ref PARAM: Regex = Regex::new(r"p\d+").unwrap();
-                    static ref STACK: Regex = Regex::new(r"s\d+").unwrap();
-                    static ref LOCAL: Regex = Regex::new(r"l\d+").unwrap();
-                    static ref CONST: Regex = Regex::new(r"const \d+").unwrap();
-                }
-                let table_idx = MEMARG.replace_all(&table_idx, "");
-                let table_idx = PARAM.replace_all(&table_idx, "<param>");
-                let table_idx = STACK.replace_all(&table_idx, "<stack>");
-                let table_idx = LOCAL.replace_all(&table_idx, "<local>");
-                let table_idx = CONST.replace_all(&table_idx, "const <const>");
-
-                *result.entry(table_idx.to_string()).or_default() += 1;
+                let table_idx = abstract_call_indirect_idx_expr(table_idx);
+                *result.entry(table_idx).or_default() += 1;
             }
-        );
+        });
     }
     let mut result = Vec::from_iter(result);
     result.sort_by_key(|(expr, count)| Reverse((*count, expr.clone())));
     result
+}
+
+// HACK Remove some stuff that is irrelevant for our analysis
+pub fn abstract_call_indirect_idx_expr(expr: &Expr) -> String {
+    let expr = expr.to_string();
+
+    lazy_static::lazy_static! {
+        static ref MEMARG: Regex = Regex::new(r"\s+offset=\d+\s+").unwrap();
+        static ref PARAM: Regex = Regex::new(r"p\d+").unwrap();
+        static ref STACK: Regex = Regex::new(r"s\d+").unwrap();
+        static ref LOCAL: Regex = Regex::new(r"l\d+").unwrap();
+        static ref CONST: Regex = Regex::new(r"const \d+").unwrap();
+    }
+    let expr = MEMARG.replace_all(&expr, "");
+    let expr = PARAM.replace_all(&expr, "<param>");
+    let expr = STACK.replace_all(&expr, "<stack>");
+    let expr = LOCAL.replace_all(&expr, "<local>");
+    let expr = CONST.replace_all(&expr, "const <const>");
+
+    expr.to_string()
 }
