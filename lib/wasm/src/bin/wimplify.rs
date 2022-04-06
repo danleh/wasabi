64a49371973e55369d5ac31b0dd015ad9eb7ea0a
Daniel Lehmann <mail@dlehmann.eu>
2022-04-06 23:24:26 +0200

    minor

--- lib/wasm/src/bin/wimplify.rs
+++ lib/wasm/src/bin/wimplify.rs
@@ -7,10 +7,10 @@ fn main() {
     let wasm_path = &args[1];
     let wimpl = wimpl::Module::from_wasm_file(wasm_path).unwrap();
 
+    // DEBUG
+    // println!("{:#?}", wimpl);
+
     if let Some(out_path) = args.get(2) {
         std::fs::write(out_path, wimpl.to_string()).unwrap();
     }
-    // for stmt in &wimpl.functions[0].body.0 {
-    //     println!("{}", stmt);
-    // }
 }
