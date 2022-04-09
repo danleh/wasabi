094332d9fd6740519037128de750210eda457587
Daniel Lehmann <mail@dlehmann.eu>
2022-04-09 22:12:40 +0200

    also dump the encoded binary in the test comparing old vs new encoder

--- lib/wasm/src/tests.rs
+++ lib/wasm/src/tests.rs
@@ -33,10 +33,12 @@ fn wasmparser_equal_old_parser() {
         let mut binary_old = Vec::new();
         let binary_size_old = module_new.to_bytes(&mut binary_old)
             .expect(&format!("could not encode valid wasm file '{}'", path.display()));
+        std::fs::write("bin-old.wasm", &binary_old).unwrap();
 
         let mut binary_new = Vec::new();
         let binary_size_new = module_new.to_bytes_wasmparser(&mut binary_new)
             .expect(&format!("could not encode valid wasm file '{}'", path.display()));
+        std::fs::write("bin-new.wasm", &binary_new).unwrap();
 
         assert_eq!(binary_size_new, binary_size_old, "difference in encoded binary length, left = wasmparser, right = old");
         assert_eq!(binary_new, binary_old, "difference in encoded binary bytes, left = wasmparser, right = old");
