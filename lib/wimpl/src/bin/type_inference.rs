2d0d50d89ee3e2f651b82292178ec37ce0db810f
Daniel Lehmann <mail@dlehmann.eu>
2022-09-26 18:23:53 +0200

    move type_inference executable to own wimpl crate

--- lib/wimpl/src/bin/type_inference.rs
+++ lib/wimpl/src/bin/type_inference.rs
@@ -4,12 +4,10 @@ use std::{
 };
 
 use rustc_hash::FxHashMap;
-use wasm::{
-    wimpl::{self, analyze::{param_exprs, collect_call_indirect_args, collect_call_indirect_idx_expr, print_map_count}, ExprKind, Function, Module, StmtKind, Var, FunctionId, Expr, traverse::VisitOptionBodyExt},
-    ValType, highlevel::{FunctionType, MemoryOp},
-};
 
+use wasm::{ValType, highlevel::{FunctionType, MemoryOp}};
 use wasm::highlevel::{BinaryOp, UnaryOp, LoadOp, StoreOp};
+use wimpl::{self, analyze::{param_exprs, collect_call_indirect_args, collect_call_indirect_idx_expr, print_map_count}, ExprKind, Function, Module, StmtKind, Var, FunctionId, Expr, traverse::VisitOptionBodyExt};
 
 use TypeConstraint::*;
 use ValType::*;
