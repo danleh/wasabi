7cc126aae84e5ed225255637d5da430733cd943b
Daniel Lehmann <mail@dlehmann.eu>
2022-12-28 23:24:59 +0100

    add build script, reorganize

--- test-inputs/issues/8/runtime-behavior-test/run-in-v8.sh
+++ test-inputs/issues/8/runtime-behavior-test/run-in-v8.sh
@@ -1,19 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -e
-
-for wat_file in *.wat; do 
-  wat2wasm $wat_file
-done
-
-for wasm_file in *.wasm; do 
+for wasm_file in build/*.wasm; do
   wasabi $wasm_file --hooks=begin,end wasabi
 done
 
 cp wasabi/*.js .
 cp wasabi/*.wasm .
 
-for wasm_file in *.wasm; do 
+for wasm_file in *.wasm; do
   echo testing $wasm_file ...
   $V8_HOME/out/x64.release/d8 assert-begin-end-matching.js -- ${wasm_file%.wasm}
 done
@@ -21,5 +15,3 @@ done
 echo "+---------+"
 echo "| SUCCESS |"
 echo "+---------+"
-
-
